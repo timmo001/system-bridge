7b1d5d6ae163e85001f82db496350b6baee780d4
Aidan Timson <contact@timmo.xyz>
2021-02-08 18:29:31 +0000

    Remove duplicate flags

--- src/app/services/info/cpu.ts
+++ src/app/services/info/cpu.ts
@@ -2,7 +2,6 @@ import si, { Systeminformation } from "systeminformation";
 
 export interface CpuInfo {
   cpu: Systeminformation.CpuData;
-  flags: string;
   currentSpeed: Systeminformation.CpuCurrentSpeedData;
   temperature: Systeminformation.CpuTemperatureData;
 }
@@ -11,7 +10,6 @@ export default class CpuInfoService {
   async find(): Promise<CpuInfo> {
     return {
       cpu: await si.cpu(),
-      flags: await si.cpuFlags(),
       currentSpeed: await si.cpuCurrentSpeed(),
       temperature: await si.cpuTemperature(),
     };
