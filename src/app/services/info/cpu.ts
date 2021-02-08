1717f9251bb7b445d0fafe3e1581cc62de66b3b3
Aidan Timson <contact@timmo.xyz>
2021-02-08 18:30:57 +0000

    Add cache data

--- src/app/services/info/cpu.ts
+++ src/app/services/info/cpu.ts
@@ -1,6 +1,7 @@
 import si, { Systeminformation } from "systeminformation";
 
 export interface CpuInfo {
+  cache: Systeminformation.CpuCacheData;
   cpu: Systeminformation.CpuData;
   currentSpeed: Systeminformation.CpuCurrentSpeedData;
   temperature: Systeminformation.CpuTemperatureData;
@@ -9,6 +10,7 @@ export interface CpuInfo {
 export default class CpuInfoService {
   async find(): Promise<CpuInfo> {
     return {
+      cache: await si.cpuCache(),
       cpu: await si.cpu(),
       currentSpeed: await si.cpuCurrentSpeed(),
       temperature: await si.cpuTemperature(),
