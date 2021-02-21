7a8a4b213baae2a7573d5cbb3e4bf50fb5bc50ee
Aidan Timson <contact@timmo.xyz>
2021-02-21 11:59:43 +0000

    Add check for access

--- src/services/bluetooth/bluetooth.class.ts
+++ src/services/bluetooth/bluetooth.class.ts
@@ -1,6 +1,8 @@
+import fs from "fs";
 import si from "systeminformation";
 
 import { Application } from "../../declarations";
+import logger from "../../logger";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ServiceOptions {}
@@ -17,6 +19,16 @@ export class Bluetooth {
   // BUSTED: async find(): Promise<Systeminformation.BluetoothDeviceData> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async find(): Promise<any> {
-    return await si.bluetoothDevices();
+    try {
+      if ((await si.osInfo()).platform === "linux")
+        fs.accessSync(
+          "/var/lib/bluetooth",
+          fs.constants.F_OK | fs.constants.W_OK
+        );
+      return await si.bluetoothDevices();
+    } catch (e) {
+      logger.warn(e);
+    }
+    return {};
   }
 }
