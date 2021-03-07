a2bfb381461d6aca360a49ba8da254be003472e8
Aidan Timson <contact@timmo.xyz>
2021-03-07 01:27:55 +0000

    Add supported methods to information endpoint

--- src/services/information/information.class.ts
+++ src/services/information/information.class.ts
@@ -17,36 +17,77 @@ export class Information {
 
   async find(): Promise<Data> {
     return {
-      audio: { description: "Audio Information", endpoint: "/audio" },
+      audio: {
+        description: "Audio Information",
+        endpoint: "/audio",
+        supportedMethods: ["GET"],
+      },
       battery: { description: "Battery Information", endpoint: "/battery" },
       bluetooth: {
         description: "Bluetooth Information",
         endpoint: "/bluetooth",
+        supportedMethods: ["GET"],
       },
-      command: { description: "Run a System Command", endpoint: "/command" },
-      cpu: { description: "CPU Information", endpoint: "/cpu" },
-      docs: { description: "OpenAPI Docs", endpoint: "/docs" },
+      command: {
+        description: "Run a System Command",
+        endpoint: "/command",
+        supportedMethods: ["POST"],
+      },
+      cpu: {
+        description: "CPU Information",
+        endpoint: "/cpu",
+        supportedMethods: ["GET"],
+      },
+      docs: {
+        description: "OpenAPI Docs",
+        endpoint: "/docs",
+        supportedMethods: ["GET"],
+      },
       filesystem: {
         description: "Filesystem Information",
         endpoint: "/filesystem",
+        supportedMethods: ["GET"],
       },
-      graphics: { description: "Graphics Information", endpoint: "/graphics" },
+      graphics: {
+        description: "Graphics Information",
+        endpoint: "/graphics",
+        supportedMethods: ["GET"],
+      },
       information: {
         description: "Endpoint Information (You are Here)",
         endpoint: "/information",
+        supportedMethods: ["GET"],
       },
-      memory: { description: "Memory Information", endpoint: "/memory" },
-      network: { description: "Network Information", endpoint: "/network" },
+      memory: {
+        description: "Memory Information",
+        endpoint: "/memory",
+        supportedMethods: ["GET"],
+      },
+      network: {
+        description: "Network Information",
+        endpoint: "/network",
+        supportedMethods: ["GET"],
+      },
       open: {
         description: "Open a URL or file using the default application",
         endpoint: "/open",
+        supportedMethods: ["POST"],
       },
-      os: { description: "OS Information", endpoint: "/os" },
+      os: {
+        description: "OS Information",
+        endpoint: "/os",
+        supportedMethods: ["GET"],
+      },
       processes: {
         description: "Processes Information",
         endpoint: "/processes",
+        supportedMethods: ["GET"],
       },
-      system: { description: "System Information", endpoint: "/system" },
+      system: {
+        description: "System Information",
+        endpoint: "/system",
+        supportedMethods: ["GET"],
+      },
     };
   }
 }
