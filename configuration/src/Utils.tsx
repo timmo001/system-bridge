fab686f7131048f63ff266e1f689b435b179d987
Aidan Timson <contact@timmo.xyz>
2021-03-07 14:46:40 +0000

    Add connection details to settings

--- configuration/src/Utils.tsx
+++ configuration/src/Utils.tsx
@@ -37,3 +37,11 @@ export const useSettings = (): [
   if (setSettings === null) throw new Error(); // this will make setSettings non-null
   return [settings, setSettings];
 };
+
+export function handleCopyToClipboard(value: string) {
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state === "granted" || result.state === "prompt") {
+      navigator.clipboard.writeText(value);
+    }
+  });
+}
