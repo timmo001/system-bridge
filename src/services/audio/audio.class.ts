160a664011bba05de217069cac1554e5a5e3fb68
Aidan Timson <contact@timmo.xyz>
2021-03-27 15:48:30 +0000

    Add audio controls
    mute, volume

--- src/services/audio/audio.class.ts
+++ src/services/audio/audio.class.ts
@@ -1,9 +1,21 @@
 import si, { Systeminformation } from "systeminformation";
+import loudness from "loudness";
 
 import { Application } from "../../declarations";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ServiceOptions {}
+
+export interface AudioInfo {
+  current: { muted: boolean; volume: number };
+  devices: Systeminformation.AudioData[];
+}
+
+export type AudioUpdateId = "mute" | "volume" | "volumeDown" | "volumeUp";
+
+export interface AudioUpdateData {
+  value?: boolean | number;
+}
 
 export class Audio {
   app: Application;
@@ -14,7 +26,46 @@ export class Audio {
     this.app = app;
   }
 
-  async find(): Promise<Systeminformation.AudioData[]> {
-    return await si.audio();
+  async find(): Promise<AudioInfo> {
+    return {
+      current: {
+        muted: await loudness.getMuted(),
+        volume: await loudness.getVolume(),
+      },
+      devices: await si.audio(),
+    };
+  }
+
+  async update(id: AudioUpdateId, data: AudioUpdateData): Promise<AudioInfo> {
+    const currentVolume: number = await loudness.getVolume();
+    switch (id) {
+      default:
+        break;
+      case "mute":
+        if (typeof data.value === "boolean")
+          await loudness.setMuted(data.value);
+        break;
+      case "volume":
+        if (typeof data.value === "number")
+          await loudness.setVolume(data.value);
+        break;
+      case "volumeDown":
+        if (typeof data.value === "number")
+          await loudness.setVolume(currentVolume - data.value);
+        else await loudness.setVolume(currentVolume - 5);
+        break;
+      case "volumeUp":
+        if (typeof data.value === "number")
+          await loudness.setVolume(currentVolume + data.value);
+        else await loudness.setVolume(currentVolume + 5);
+        break;
+    }
+    return {
+      current: {
+        muted: await loudness.getMuted(),
+        volume: await loudness.getVolume(),
+      },
+      devices: await si.audio(),
+    };
   }
 }
