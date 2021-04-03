f421f45002cce3827ed65af433880afbe5b758c4
Aidan Timson <contact@timmo.xyz>
2021-04-03 20:52:01 +0100

    Remove script and use shx

--- scripts/copy-frontend-to-public.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-fs.renameSync(
-  path.resolve(__dirname, "../frontend/build"),
-  path.resolve(__dirname, "../public/frontend")
-);
-fs.writeFileSync(path.resolve(__dirname, "../public/frontend/.gitkeep"), "");
