452c0d4ac57caa282a42880e3437afdad24d940a
Aidan Timson <contact@timmo.xyz>
2021-04-03 11:53:14 +0100

    Cleanup

--- rtc/src/Main.tsx
+++ rtc/src/Main.tsx
@@ -1,13 +1,54 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
+import Peer from "peerjs";
 
-import Webcam from "./Webcam";
+import { Configuration } from "../../src/configuration";
+import { useSettings } from "./Utils";
 
 function Main(): ReactElement {
-  return (
-    <>
-      <Webcam />
-    </>
-  );
+  const [settings, setSettings] = useSettings();
+
+  useEffect(() => {
+    if (!settings) {
+      window.api.ipcRendererOn("set-settings", (_event, args) => {
+        console.log("set-settings:", args);
+        const s: Configuration = args;
+        setSettings(s);
+      });
+      window.api.ipcRendererSend("get-settings");
+    }
+  }, [settings, setSettings]);
+
+  useEffect(() => {
+    (async () => {
+      console.log("Create peer");
+      const peer = new Peer();
+
+      peer.on("open", (id) => {
+        console.log("My peer ID is: " + id);
+      });
+
+      peer.on("call", async (call) => {
+        try {
+          const mediaStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+          });
+          // Answer the call, providing our mediaStream
+          call.answer(mediaStream);
+
+          // const video = document.getElementById(
+          //   "video-stream"
+          // ) as HTMLMediaElement;
+          // video.srcObject = mediaStream;
+          // video.load();
+        } catch (err) {
+          console.error(err);
+        }
+      });
+    })();
+  }, []);
+
+  return <></>;
 }
 
 export default Main;
