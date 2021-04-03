ea3559f469bc6f81e8453a18e6fdfb946cb75cc5
Aidan Timson <contact@timmo.xyz>
2021-04-03 14:56:32 +0100

    Close stream if no connections are present

--- rtc/src/Main.tsx
+++ rtc/src/Main.tsx
@@ -5,7 +5,9 @@ import Peer from "peerjs";
 import { Configuration } from "../../src/configuration";
 import { useSettings } from "./Utils";
 
-let peer: Peer;
+let peer: Peer,
+  peerConnectionInterval: NodeJS.Timeout,
+  mediaStream: MediaStream | null;
 function Main(): ReactElement {
   const [settings, setSettings] = useSettings();
 
@@ -35,28 +37,46 @@ function Main(): ReactElement {
         peer.on("open", (id) => {
           console.log("My peer ID is: " + id);
         });
-
-        peer.on("call", async (call) => {
+        peer.on("connection", (dataConnection: Peer.DataConnection) => {
+          console.log("New connection from ", dataConnection.peer);
+        });
+        peer.on("call", async (mediaConnection: Peer.MediaConnection) => {
           try {
-            const mediaStream = await navigator.mediaDevices.getUserMedia({
+            mediaStream = await navigator.mediaDevices.getUserMedia({
               audio: true,
               video: true,
             });
             // Answer the call, providing our mediaStream
-            call.answer(mediaStream);
+            mediaConnection.answer(mediaStream);
 
             const video = document.getElementById(
               "video-stream"
             ) as HTMLMediaElement;
             video.srcObject = mediaStream;
-            video.load();
+            video.play();
           } catch (err) {
             console.error(err);
           }
+
+          peerConnectionInterval = setInterval(() => {
+            peer.listAllPeers((peerIds: string[]) => {
+              if (peerIds.length === 1) {
+                console.log("No more peers. Closing stream");
+                const video = document.getElementById(
+                  "video-stream"
+                ) as HTMLMediaElement;
+                video.srcObject = null;
+                mediaStream?.getTracks().forEach((track) => track.stop());
+                mediaStream = null;
+                clearInterval(peerConnectionInterval);
+              }
+            });
+          }, 2000);
         });
       })();
     return () => {
       if (peer) {
+        if (peerConnectionInterval) clearInterval(peerConnectionInterval);
         peer.disconnect();
         peer.destroy();
       }
