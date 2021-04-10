424f3fae08b2979d308c1d92c5e0c651a3c8452b
Aidan Timson <contact@timmo.xyz>
2021-04-10 14:08:08 +0100

    Use mocha

---
+++ test/api/app.test.ts
@@ -0,0 +1,64 @@
+import { Server } from "http";
+import assert from "assert";
+import axios from "axios";
+
+import app from "../../src/api/app";
+
+const getUrl = (pathname?: string): string => {
+  const u = new URL("http://localhost:9170");
+  u.pathname = pathname;
+  return u.toString();
+};
+
+describe("Feathers application tests", () => {
+  let server: Server;
+
+  before(function (done) {
+    server = app.listen(9170);
+    server.once("listening", () => done());
+  });
+
+  after(function (done) {
+    server.close(done);
+  });
+
+  // it("starts and shows the index page", async () => {
+  //   const { data } = await axios.get(getUrl());
+  //   assert.ok(data.indexOf('<html lang="en">') !== -1);
+  // });
+
+  describe("404", function () {
+    it("shows a 404 HTML page", async () => {
+      try {
+        await axios.get(getUrl("path/to/nowhere"), {
+          headers: {
+            Accept: "text/html",
+          },
+        });
+        assert.fail("should never get here");
+      } catch (error) {
+        const { response } = error;
+
+        assert.strictEqual(response.status, 404);
+        console.log(response.data);
+        assert.ok(response.data.indexOf("<html>") !== -1);
+      }
+    });
+
+    it("shows a 404 JSON error without stack trace", async () => {
+      try {
+        await axios.get(getUrl("path/to/nowhere"));
+        assert.fail("should never get here");
+      } catch (error) {
+        const { response } = error;
+
+        console.log(response);
+
+        assert.strictEqual(response.status, 404);
+        assert.strictEqual(response.data.code, 404);
+        assert.strictEqual(response.data.message, "Page not found");
+        assert.strictEqual(response.data.name, "NotFound");
+      }
+    });
+  });
+});
