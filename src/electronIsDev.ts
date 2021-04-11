7c61679fa4ce21527edac80478a1adfbc921d72a
Aidan Timson <contact@timmo.xyz>
2021-04-11 16:01:55 +0100

    Use own variant of electron_is_dev

--- src/electronIsDev.ts
+++ src/electronIsDev.ts
@@ -1,9 +1,7 @@
-const electronIsDev = async (): Promise<boolean> => {
-  try {
-    return (await import("electron-is-dev")).default;
-  } catch (e) {
-    return false;
-  }
+const electronIsDev = (): boolean => {
+  const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV || "1", 10) === 1;
+  const isDev = getFromEnv || process.env.NODE_ENV === "development";
+  return isDev;
 };
 
 export default electronIsDev;
