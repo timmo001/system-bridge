e30b318377d00ed5e5b4cf2b3972af15a681aa70
Aidan Timson <contact@timmo.xyz>
2021-04-11 16:23:04 +0100

    Assume prod unless stated

--- src/electronIsDev.ts
+++ src/electronIsDev.ts
@@ -1,5 +1,5 @@
 const electronIsDev = (): boolean => {
-  const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV || "1", 10) === 1;
+  const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV || "0", 10) === 1;
   const isDev = getFromEnv || process.env.NODE_ENV === "development";
   return isDev;
 };
