5ce731eb66ee49469e20b6961a2bd654a3f42e37
Aidan Timson <contact@timmo.xyz>
2021-04-18 21:42:54 +0100

    Cleanup

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from "@nestjs/common";
 import { WinstonModule } from "nest-winston";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import logger from "../logger";
 
 @Module({
   imports: [WinstonModule.forRoot(logger)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
