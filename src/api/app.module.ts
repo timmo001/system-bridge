3c01049ab950c02ecd05bef0927616d5cf89b152
Aidan Timson <contact@timmo.xyz>
2021-04-19 17:49:09 +0100

    Add bluetooth

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { WinstonModule } from "nest-winston";
+
 import { AudioModule } from "./audio/audio.module";
 import { BatteryModule } from "./battery/battery.module";
+import { BluetoothModule } from "./bluetooth/bluetooth.module";
 import { MediaModule } from "./media/media.module";
 import logger from "../logger";
 
@@ -10,6 +12,7 @@ import logger from "../logger";
     WinstonModule.forRoot(logger),
     AudioModule,
     BatteryModule,
+    BluetoothModule,
     MediaModule,
   ],
   controllers: [],
