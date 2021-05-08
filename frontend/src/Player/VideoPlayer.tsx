11d3e78b730ee49195bcdf7f7c2127ca6f15aedb
Aidan Timson <contact@timmo.xyz>
2021-05-08 22:51:27 +0100

    Cleanup

--- frontend/src/Player/VideoPlayer.tsx
+++ frontend/src/Player/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useMemo } from "react";
 import ReactPlayer from "react-player/lazy";
 
 import { usePlayer, VideoSource } from "./Utils";
