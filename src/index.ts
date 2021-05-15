509307571df5e5baa3e3b3a88d0f4ba214be824b
Aidan Timson <contact@timmo.xyz>
2021-05-15 16:57:52 +0100

    Check for updates

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {
   ipcMain,
   Menu,
   MenuItemConstructorOptions,
+  Notification,
   protocol,
   shell,
   Tray,
@@ -14,6 +15,7 @@ import autoUpdater from "update-electron-app";
 import devTools, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 import execa from "execa";
 import queryString from "query-string";
+import semver from "semver";
 import si, { Systeminformation } from "systeminformation";
 import WebSocket from "ws";
 
@@ -28,6 +30,27 @@ import { closePlayerWindow, PlayerStatus } from "./player";
 import { startServer, stopServer } from "./api";
 import electronIsDev from "./electronIsDev";
 import logger from "./logger";
+import axios from "axios";
+
+export interface ApplicationInfo {
+  address: string;
+  fqdn: string;
+  host: string;
+  ip: string;
+  mac: string;
+  port: number;
+  uuid: string;
+  version: string;
+  websocketAddress: string;
+  websocketPort: number;
+}
+
+export interface ApplicationUpdate {
+  url: string;
+  version: { current: string; new: string };
+}
+
+export const GITHUB_REPOSITORY = "timmo001/system-bridge";
 
 logger.info(
   `System Bridge ${app.getVersion()}: ${JSON.stringify(process.argv)}`
@@ -152,6 +175,29 @@ const helpMenu: Array<MenuItemConstructorOptions> = [
   },
 ];
 
+const contextMenuTemplate: Array<MenuItemConstructorOptions> = [
+  { label: "Settings", type: "normal", click: showConfigurationWindow },
+  { type: "separator" },
+  {
+    label: "Close Active Media Player",
+    type: "normal",
+    click: closePlayerWindow,
+  },
+  { type: "separator" },
+  {
+    id: "version-latest",
+    label: "Latest Version",
+    type: "normal",
+    click: () =>
+      shell.openExternal(
+        "https://github.com/timmo001/system-bridge/releases/latest"
+      ),
+  },
+  ...helpMenu,
+  { type: "separator" },
+  { label: "Quit", type: "normal", click: quitApp },
+];
+
 async function setAppConfig(): Promise<void> {
   const config = getSettings();
   if (!isDev) {
@@ -261,32 +307,24 @@ app.on("activate", (): void => {
   }
 });
 
-app.whenReady().then(async (): Promise<void> => {
-  tray = new Tray(appSmallIconPath);
-  const contextMenu = Menu.buildFromTemplate([
-    { label: "Settings", type: "normal", click: showConfigurationWindow },
-    { type: "separator" },
-    {
-      label: "Close Active Media Player",
-      type: "normal",
-      click: closePlayerWindow,
-    },
-    { type: "separator" },
-    ...helpMenu,
-    { type: "separator" },
-    { label: "Quit", type: "normal", click: quitApp },
-  ]);
-  tray.setToolTip("System Bridge");
-  tray.setContextMenu(contextMenu);
-  tray.setIgnoreDoubleClickEvents(true);
-  tray.on("double-click", showConfigurationWindow);
+app.whenReady().then(
+  async (): Promise<void> => {
+    tray = new Tray(appSmallIconPath);
+    tray.setToolTip("System Bridge");
+    tray.setContextMenu(Menu.buildFromTemplate(contextMenuTemplate));
+    tray.setIgnoreDoubleClickEvents(true);
+    tray.on("double-click", showConfigurationWindow);
 
-  startServer();
-  ws = await wsSendEvent({ name: "startup", data: "started" }, ws, true);
-  ipcMain.emit("get-app-information");
-});
+    startServer();
+    ws = await wsSendEvent({ name: "startup", data: "started" }, ws, true);
+    ipcMain.emit("get-app-information");
+    ipcMain.emit("update-check");
+  }
+);
 
-ipcMain.on("get-app-information", async (event): Promise<void> => {
+export async function getAppInformation(): Promise<
+  ApplicationInfo | undefined
+> {
   const settings = getSettings();
   const port: number =
     typeof settings?.network.items?.port?.value === "number"
@@ -299,64 +337,148 @@ ipcMain.on("get-app-information", async (event): Promise<void> => {
   const osInfo: Systeminformation.OsData = await si.osInfo();
   const uuidInfo: Systeminformation.UuidData = await si.uuid();
   const defaultInterface: string = await si.networkInterfaceDefault();
-  const networkInterface: Systeminformation.NetworkInterfacesData | undefined =
-    (await si.networkInterfaces()).find(
-      (ni: Systeminformation.NetworkInterfacesData) =>
-        ni.iface === defaultInterface
-    );
+  const networkInterface:
+    | Systeminformation.NetworkInterfacesData
+    | undefined = (await si.networkInterfaces()).find(
+    (ni: Systeminformation.NetworkInterfacesData) =>
+      ni.iface === defaultInterface
+  );
 
-  const data = {
-    address: `http://${osInfo.fqdn}:${port}`,
-    fqdn: osInfo.fqdn,
-    host: osInfo.hostname,
-    ip: networkInterface?.ip4,
-    mac: networkInterface?.mac,
-    port,
-    uuid: uuidInfo.os,
-    version: app.getVersion(),
-    websocketAddress: `ws://${osInfo.fqdn}:${websocketPort}`,
-    websocketPort,
-  };
-  logger.info(`App information: ${JSON.stringify(data)}`);
-  event?.sender?.send("app-information", data);
-  ws = await wsSendEvent({ name: "app-information", data: data }, ws, true);
-});
+  if (networkInterface) {
+    const data = {
+      address: `http://${osInfo.fqdn}:${port}`,
+      fqdn: osInfo.fqdn,
+      host: osInfo.hostname,
+      ip: networkInterface.ip4,
+      mac: networkInterface.mac,
+      port,
+      uuid: uuidInfo.os,
+      version: app.getVersion(),
+      websocketAddress: `ws://${osInfo.fqdn}:${websocketPort}`,
+      websocketPort,
+    };
+    logger.info(`App information: ${JSON.stringify(data)}`);
 
-ipcMain.on("open-url", async (event, arg): Promise<void> => {
-  shell.openExternal(arg);
-  event?.sender?.send("opened-url", arg);
-});
+    return data;
+  }
+  return undefined;
+}
 
-ipcMain.on("open-settings", async (event): Promise<void> => {
-  showConfigurationWindow();
-  event?.sender?.send("opened-settings");
-});
+export async function checkForUpdates(): Promise<
+  ApplicationUpdate | undefined
+> {
+  const response = await axios.get<{
+    html_url: string;
+    prerelease: boolean;
+    tag_name: string;
+  }>(`https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/latest`);
+  if (
+    response &&
+    response.status < 400 &&
+    response.data?.prerelease === false
+  ) {
+    const versionCurrent = semver.clean(app.getVersion()) as string;
+    const versionNew = semver.clean(response.data.tag_name) as string;
+    const available = semver.lt(versionCurrent, versionNew);
+    const data = {
+      url: response.data.html_url,
+      version: { current: versionCurrent, new: versionNew },
+    };
+    logger.info(`checkForUpdates: ${JSON.stringify(data)}`);
+    if (available) return data;
+  }
+  return undefined;
+}
 
-ipcMain.on("get-settings", async (event): Promise<void> => {
-  event?.sender?.send("set-settings", getSettings());
-});
+ipcMain.on(
+  "get-app-information",
+  async (event): Promise<void> => {
+    const data = getAppInformation();
+    event?.sender?.send("app-information", data);
+    ws = await wsSendEvent({ name: "app-information", data: data }, ws, true);
+  }
+);
 
-ipcMain.on("update-setting", async (event, args): Promise<void> => {
-  logger.debug(`update-setting: ${args[0]}, ${args[1]}`);
-  await setSetting(args[0], args[1]);
-  await setAppConfig();
-  event?.sender?.send("updated-setting", args);
-  ipcMain.emit("updated-setting", args);
-});
+ipcMain.on(
+  "update-check",
+  async (event): Promise<void> => {
+    const update = await checkForUpdates();
+    if (update) {
+      event?.sender?.send("update-available", update);
+      ws = await wsSendEvent(
+        { name: "update-available", data: update },
+        ws,
+        true
+      );
+      contextMenuTemplate[
+        contextMenuTemplate.findIndex(
+          (mi: MenuItemConstructorOptions) => mi.id === "version-latest"
+        )
+      ].label = `Version ${update.version.new} Avaliable!`;
+      tray.setContextMenu(Menu.buildFromTemplate(contextMenuTemplate));
+      const notification = new Notification({
+        title: "System Bridge - Update Avaliable!",
+        body: `Version ${update.version.new} is available.`,
+      });
+      notification.on("click", () => shell.openExternal(update.url));
+      notification.show();
+    }
+  }
+);
 
-ipcMain.on("restart-app", async (event): Promise<void> => {
-  event?.sender?.send("restarting-app");
-  ipcMain.emit("restarting-app");
-  logger.debug("restarting-app");
-  app.relaunch();
-});
+ipcMain.on(
+  "open-url",
+  async (event, arg): Promise<void> => {
+    shell.openExternal(arg);
+    event?.sender?.send("opened-url", arg);
+  }
+);
 
-ipcMain.on("restart-server", async (event): Promise<void> => {
-  event?.sender?.send("restarting-server");
-  ipcMain.emit("restarting-server");
-  await stopServer();
-  setTimeout(() => startServer(), 2000);
-});
+ipcMain.on(
+  "open-settings",
+  async (event): Promise<void> => {
+    showConfigurationWindow();
+    event?.sender?.send("opened-settings");
+  }
+);
+
+ipcMain.on(
+  "get-settings",
+  async (event): Promise<void> => {
+    event?.sender?.send("set-settings", getSettings());
+  }
+);
+
+ipcMain.on(
+  "update-setting",
+  async (event, args): Promise<void> => {
+    logger.debug(`update-setting: ${args[0]}, ${args[1]}`);
+    await setSetting(args[0], args[1]);
+    await setAppConfig();
+    event?.sender?.send("updated-setting", args);
+    ipcMain.emit("updated-setting", args);
+  }
+);
+
+ipcMain.on(
+  "restart-app",
+  async (event): Promise<void> => {
+    event?.sender?.send("restarting-app");
+    ipcMain.emit("restarting-app");
+    logger.debug("restarting-app");
+    app.relaunch();
+  }
+);
+
+ipcMain.on(
+  "restart-server",
+  async (event): Promise<void> => {
+    event?.sender?.send("restarting-server");
+    ipcMain.emit("restarting-server");
+    await stopServer();
+    setTimeout(() => startServer(), 2000);
+  }
+);
 
 ipcMain.on("window-show", (event) => {
   const window = BrowserWindow.fromWebContents(event?.sender);
