fc0dc622de7a1be3deb36cc66aeff7ceb1f4ff7c
Aidan Timson <contact@timmo.xyz>
2021-05-15 12:16:53 +0100

    Send to authenticated clients

--- src/api/events/events.gateway.ts
+++ src/api/events/events.gateway.ts
@@ -1,10 +1,12 @@
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WsResponse,
 } from "@nestjs/websockets";
 import { UseGuards } from "@nestjs/common";
+import WebSocket from "ws";
 
 import { Event } from "./entities/event.entity";
 import { getSettings } from "../../common";
@@ -20,9 +22,23 @@ const port: number =
 
 @WebSocketGateway(port, { transports: ["websocket"] })
 export class EventsGateway {
+  private authenticatedClients: Array<WebSocket> = [];
+
+  @UseGuards(WsAuthGuard)
+  @SubscribeMessage("authenticate")
+  async handleAuthenticate(
+    @ConnectedSocket() client: WebSocket
+  ): Promise<WsResponse<boolean>> {
+    this.authenticatedClients.push(client);
+    return { event: "authenticate", data: true };
+  }
+
   @UseGuards(WsAuthGuard)
   @SubscribeMessage("events")
   handleEvent(@MessageBody() { data }: { data: Event }): WsResponse<Event> {
-    return { event: "events", data };
+    this.authenticatedClients.forEach((ws: WebSocket) =>
+      ws.send(JSON.stringify({ event: "events", data }))
+    );
+    return { event: "event-sent", data };
   }
 }
