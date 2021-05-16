5ecab91144efe780667f9c7aea21b86c419aa408
Aidan Timson <contact@timmo.xyz>
2021-05-16 20:07:04 +0100

    Add logs for events

--- src/api/events/events.gateway.ts
+++ src/api/events/events.gateway.ts
@@ -11,6 +11,7 @@ import WebSocket from "ws";
 import { Event } from "./entities/event.entity";
 import { getSettings } from "../../common";
 import { WsAuthGuard } from "../wsAuth.guard";
+import logger from "../../logger";
 
 const settings = getSettings();
 const networkSettings = settings?.network.items;
@@ -29,6 +30,7 @@ export class EventsGateway {
   async handleAuthenticate(
     @ConnectedSocket() client: WebSocket
   ): Promise<WsResponse<boolean>> {
+    logger.info("WebSocket - New client registered");
     this.authenticatedClients.push(client);
     return { event: "registered-listener", data: true };
   }
@@ -36,6 +38,7 @@ export class EventsGateway {
   @UseGuards(WsAuthGuard)
   @SubscribeMessage("events")
   handleEvent(@MessageBody() { data }: { data: Event }): WsResponse<Event> {
+    logger.info(`WebSocket - New event: ${data.name}`);
     this.authenticatedClients.forEach((ws: WebSocket) =>
       ws.send(JSON.stringify({ event: "events", data }))
     );
