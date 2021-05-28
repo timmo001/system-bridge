3c7044df34e5cbad68e7f5ca925149b11aa282fe
Aidan Timson <contact@timmo.xyz>
2021-05-28 17:53:27 +0100

    Find process by name

--- src/api/processes/processes.controller.ts
+++ src/api/processes/processes.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Query,
+} from "@nestjs/common";
 
-import { Processes } from "./entities/processes.entity";
+import { Process, Processes } from "./entities/processes.entity";
 import { ProcessesService } from "./processes.service";
 
 @Controller("processes")
@@ -10,5 +17,22 @@ export class ProcessesController {
   @Get()
   async findAll(): Promise<Processes> {
     return await this.processesService.findAll();
+  }
+
+  @Get(":name")
+  async find(
+    @Param("name") name: string,
+    @Query("exact") exact = false
+  ): Promise<Process[]> {
+    const data = await this.processesService.find(name, exact);
+    if (!data || data.length === 0)
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: `Could not process including the name: "${name}"`,
+        },
+        HttpStatus.BAD_REQUEST
+      );
+    return data;
   }
 }
--- src/api/processes/processes.service.ts
+++ src/api/processes/processes.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import si from "systeminformation";
 
-import { Processes } from "./entities/processes.entity";
+import { Process, Processes } from "./entities/processes.entity";
 
 @Injectable()
 export class ProcessesService {
   async findAll(): Promise<Processes> {
     return { ...(await si.processes()), load: await si.currentLoad() };
+  }
+
+  async find(name: string, exact: boolean): Promise<Process[] | undefined> {
+    const data = await this.findAll();
+    return data.list.filter((process: Process) =>
+      exact
+        ? process.name.toLowerCase() === name.toLowerCase()
+        : process.name.toLowerCase().includes(name.toLowerCase())
+    );
   }
 }
