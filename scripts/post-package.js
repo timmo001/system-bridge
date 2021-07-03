c4c9ca6ad7e337b0fe54297833ee0a2d0d1b02cd
Aidan Timson <contact@timmo.xyz>
2021-07-03 21:50:59 +0100

    Linux Packages (#461)
    * Add nfpm file
    
    * Update
    
    * Handle linux in CI
    
    * Add yq to release drafter
    
    * Give name
    
    * Cleanup
    
    * Install yq
    
    * Fix yq
    
    * Update version to v2.0.0
    
    * Use own script
    
    * Add apk file
    
    * Rename
    
    * Install nfpm
    
    * Install from deb file
    
    * Move out upload to before creating setup files
    
    * Upload after creation
    
    * Add dependency
    
    * Fix package path
    
    * Remove package.json from package
    
    * Build based on platform
    
    * Make all paths relative to current
    
    * Disable concurrency
    
    * Fix
    
    * Create desktop file
    
    * Add path to desktop file
    
    * Check if item exists
    
    Co-authored-by: timmo001 <timmo001@users.noreply.github.com>

--- scripts/post-package.js
+++ scripts/post-package.js
@@ -3,70 +3,77 @@ const { join } = require("path");
 
 const filePaths = [
   {
-    from: "../package.json",
-    to: "package.json",
+    from: "../LICENSE",
+    to: "../out/LICENSE",
   },
   {
-    from: "../LICENSE",
-    to: "LICENSE",
+    from: "../public/system-bridge-circle.png",
+    to: "../out/system-bridge-circle.png",
   },
   {
     from: "../node_modules/node-notifier/vendor/notifu/notifu.exe",
-    to: "notifier/notifu.exe",
+    to: "../out/notifier/notifu.exe",
+    platform: "win32",
   },
   {
     from: "../node_modules/node-notifier/vendor/notifu/notifu64.exe",
-    to: "notifier/notifu64.exe",
+    to: "../out/notifier/notifu64.exe",
+    platform: "win32",
   },
   {
     from: "../node_modules/node-notifier/vendor/terminal-notifier.app/Contents/MacOS/terminal-notifier",
-    to: "notifier/terminal-notifier",
+    to: "../out/notifier/terminal-notifier",
+    platform: "darwin",
   },
   {
     from: "../node_modules/node-notifier/vendor/snoreToast/snoretoast-x64.exe",
-    to: "notifier/snoretoast-x64.exe",
+    to: "../out/notifier/snoretoast-x64.exe",
+    platform: "win32",
   },
   {
     from: "../node_modules/node-notifier/vendor/snoreToast/snoretoast-x86.exe",
-    to: "notifier/snoretoast-x86.exe",
+    to: "../out/notifier/snoretoast-x86.exe",
+    platform: "win32",
   },
   {
     from: "../node_modules/open/xdg-open",
-    to: "xdg-open",
+    to: "../out/xdg-open",
   },
   {
     from: "../node_modules/systray2/traybin/tray_darwin_release",
-    to: "traybin/tray_darwin_release",
+    to: "../out/traybin/tray_darwin_release",
+    platform: "darwin",
   },
   {
     from: "../node_modules/systray2/traybin/tray_linux_release",
-    to: "traybin/tray_linux_release",
+    to: "../out/traybin/tray_linux_release",
+    platform: "linux",
   },
   {
     from: "../node_modules/systray2/traybin/tray_windows_release.exe",
-    to: "traybin/tray_windows_release.exe",
+    to: "../out/traybin/tray_windows_release.exe",
+    platform: "win32",
   },
 ];
 
-const nodeModulesDir = join(__dirname, "../node_modules");
-const outDir = join(__dirname, "../out");
-
-filePaths.forEach((path) => {
-  const sourceFile = join(__dirname, path.from);
-  if (existsSync(sourceFile)) {
-    const targetDir = join(
-      outDir,
-      path.to.substring(0, path.to.lastIndexOf("/"))
-    );
-    if (!existsSync(targetDir)) mkdirSync(targetDir);
-    const targetFile = join(outDir, path.to);
-    console.log(`Copy ${sourceFile} to ${targetFile}`);
-    copyFileSync(sourceFile, targetFile);
-  }
-});
+filePaths
+  .filter((path) => (path.platform ? path.platform === process.platform : true))
+  .forEach((path) => {
+    const sourceFile = join(__dirname, path.from);
+    if (existsSync(sourceFile)) {
+      const targetDir = join(
+        __dirname,
+        path.to.substring(0, path.to.lastIndexOf("/"))
+      );
+      if (!existsSync(targetDir)) mkdirSync(targetDir);
+      const targetFile = join(__dirname, path.to);
+      console.log(`Copy ${sourceFile} to ${targetFile}`);
+      copyFileSync(sourceFile, targetFile);
+    }
+  });
 
 if (process.platform === "win32")
   require("create-nodew-exe")({
-    src: join(outDir, "system-bridge.exe"),
-    dst: join(outDir, "start-system-bridge.exe"),
+    src: join(__dirname, "../out/system-bridge.exe"),
+    dst: join(__dirname, "../out/start-system-bridge.exe"),
   });
