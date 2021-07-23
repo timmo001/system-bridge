0870494b9fe674c90e3299ec72535cf343436a5d
Aidan Timson <contact@timmo.xyz>
2021-07-23 20:38:59 +0100

    Data

--- frontend/src/Data/Data.tsx
+++ frontend/src/Data/Data.tsx
@@ -6,6 +6,7 @@ import {
   Grid,
   makeStyles,
   Theme,
+  Typography,
 } from "@material-ui/core";
 
 import { Event } from "../types/event.entity";
@@ -14,7 +15,7 @@ import { WebSocketConnection } from "../Common/WebSocket";
 import Footer from "../Common/Footer";
 import Header from "../Common/Header";
 
-const dataItems = [
+const items = [
   "audio",
   "battery",
   "bluetooth",
@@ -50,51 +51,81 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 function DataComponent(): ReactElement {
-  const [allData, setData] = useState<{ [name: string]: any }>();
+  const [dataItems, setDataItems] = useState<Array<Event>>([]);
+  const [setup, setSetup] = useState<boolean>(false);
   const [settings] = useSettings();
 
   const query = useMemo(() => parsedQuery, []);
 
-  const setup = useCallback(() => {
-    setData({});
-    const ws = new WebSocketConnection(
-      Number(query.wsPort) || 9172,
-      String(query.apiKey),
-      true,
-      async () => {
-        ws.sendEvent({
-          name: "get-data",
-          data: dataItems,
-        });
-      }
-    );
-    ws.onEvent = ({ name, data }: Event) => {
-      console.log("Event:", { name, data });
+  const eventHandler = useCallback(
+    ({ name, data }: Event) => {
       if (name.includes("data-") || name.includes("observer-")) {
-        const key = dataItems.find((item: string) => name.includes(item));
-        if (key) setData({ ...allData, [key]: data });
+        const key = items.find((item: string) => name.includes(item));
+        if (key) {
+          const event = { name: key, data: data };
+          const id = dataItems.findIndex((item: Event) => item.name === key);
+          console.log("Event:", id, event);
+          if (id > -1) dataItems[id] = event;
+          else dataItems.push(event);
+          dataItems.sort((a: Event, b: Event) => a.name.localeCompare(b.name));
+          setDataItems(dataItems);
+        }
       }
-    };
-  }, [allData, query.apiKey, query.wsPort]);
+    },
+    [dataItems]
+  );
+
+  const handleSetup = useCallback(
+    (wsPort: number, apiKey: string) => {
+      console.log("Setup WebSocketConnection");
+      const ws = new WebSocketConnection(wsPort, apiKey, true, async () => {
+        ws.sendEvent({ name: "get-data", data: items });
+      });
+      // setInterval(() => {
+      //   dataItems.push({ name: "test", data: { test: Math.random() * 100 } });
+      //   setDataItems(dataItems);
+      // }, 1000);
+      ws.onEvent = eventHandler;
+    },
+    [eventHandler]
+  );
 
   useEffect(() => {
-    if (!allData) setup();
-  }, [allData, setup]);
+    if (!setup) {
+      setSetup(true);
+      handleSetup(Number(query.wsPort) || 9172, String(query.apiKey));
+    }
+  }, [setup, handleSetup, query.apiKey, query.wsPort]);
+
+  console.log("update:", dataItems);
 
   const classes = useStyles();
-
-  console.log("allData:", allData);
 
   return (
     <Container className={classes.root} maxWidth="lg">
       <Header name="Data" />
       <Grid container direction="column" spacing={2} alignItems="stretch">
-        {!settings ? (
+        {!settings || !dataItems ? (
           <Grid container direction="row" justifyContent="center">
             <CircularProgress />
           </Grid>
         ) : (
-          <Grid container direction="row" justifyContent="center"></Grid>
+          <>
+            {dataItems.map((item: Event) => (
+              <Grid key={item.name} item xs>
+                <Typography component="p" variant="body1">
+                  {item.name}
+                </Typography>
+                <Typography component="p" variant="body1">
+                  {item.data
+                    ? Array.isArray(item.data)
+                      ? JSON.stringify(item.data[0])
+                      : JSON.stringify(Object.values(item.data)[0])
+                    : "No data"}
+                </Typography>
+              </Grid>
+            ))}
+          </>
         )}
       </Grid>
       <Footer />
