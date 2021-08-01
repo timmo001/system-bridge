1a195c67fb898ac76e789890f213421085d60b0a
Aidan Timson <contact@timmo.xyz>
2021-08-01 22:08:58 +0100

    Add hardware sensors to GPU endpoint (#641)
    * Add hardware sensors to GPU endpoint
    
    * Update version to v2.4.0
    
    Co-authored-by: timmo001 <timmo001@users.noreply.github.com>

--- src/graphics/graphics.service.ts
+++ src/graphics/graphics.service.ts
@@ -1,9 +1,39 @@
+import { Hardware } from "system-bridge-windows-sensors";
 import { Injectable } from "@nestjs/common";
-import { graphics, Systeminformation } from "systeminformation";
+
+import { Graphics } from "./entities/graphics.entity";
+import { graphics } from "systeminformation";
+import logger from "../logger";
 
 @Injectable()
 export class GraphicsService {
-  async findAll(): Promise<Systeminformation.GraphicsData> {
-    return await graphics();
+  async findAll(): Promise<Graphics> {
+    const data: Graphics = {
+      ...(await graphics()),
+    };
+
+    if (process.platform === "win32") {
+      try {
+        const { getHardwareByType } = await import(
+          "system-bridge-windows-sensors"
+        );
+
+        const hardware: Hardware = (await getHardwareByType(
+          "Gpu",
+          !process.argv0.includes("node.exe"),
+          false,
+          false
+        )) as Hardware;
+
+        console.log("hardware:", typeof hardware, hardware);
+
+        if (hardware.sensors) {
+          data.hardwareSensors = hardware.sensors;
+        }
+      } catch (e) {
+        logger.error(e.message);
+      }
+    }
+    return data;
   }
 }
