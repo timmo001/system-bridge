09ed39b3e63e702d28ebc909de1792f44e889abe
Aidan Timson <contact@timmo.xyz>
2021-08-19 19:31:04 +0100

    Handle OS Service errors

--- src/api/os/os.service.ts
+++ src/api/os/os.service.ts
@@ -3,13 +3,20 @@ import { Injectable } from "@nestjs/common";
 import { osInfo, users } from "systeminformation";
 
 import { Os } from "./entities/os.entity";
+import logger from "src/logger";
 
 @Injectable()
 export class OsService {
   async findAll(): Promise<Os> {
+    let idleTime = -1;
+    try {
+      idleTime = getIdleTime();
+    } catch (e) {
+      logger.error(`OsService - Error ${e.message}`);
+    }
     return {
       ...(await osInfo()),
-      idleTime: getIdleTime(),
+      idleTime,
       users: await users(),
     };
   }
