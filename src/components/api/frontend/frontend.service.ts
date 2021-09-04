ad75e6130737d9c0c3ae9a746a24f2523cc226f2
Aidan Timson <contact@timmo.xyz>
2021-09-04 22:52:58 +0100

    Fix

--- src/components/api/frontend/frontend.service.ts
+++ src/components/api/frontend/frontend.service.ts
@@ -2,6 +2,7 @@ import { Injectable, OnModuleInit } from "@nestjs/common";
 import { NextServer } from "next/dist/server/next";
 import { Request, Response } from "express";
 import next from "next";
+import { join } from "path";
 
 @Injectable()
 export class FrontendService implements OnModuleInit {
@@ -11,7 +12,7 @@ export class FrontendService implements OnModuleInit {
     try {
       this.server = next({
         dev: false,
-        dir: "./frontend",
+        dir: join(__dirname, "../../../../frontend"),
       });
       await this.server.prepare();
     } catch (error) {
