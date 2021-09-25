95877816ebef9d9251b598780520aa057d9e0313
Aidan Timson <contact@timmo.xyz>
2021-09-25 01:43:57 +0100

    Add create file service

--- src/components/api/filesystem/filesystem.controller.ts
+++ src/components/api/filesystem/filesystem.controller.ts
@@ -1,6 +1,21 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  createParamDecorator,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Req,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
+import rawbody from "raw-body";
 
-import { Filesystem } from "./entities/filesystem.entity";
+import {
+  Filesystem,
+  FilesystemUploadResponse,
+} from "./entities/filesystem.entity";
 import { FilesystemService } from "./filesystem.service";
 import { HttpAuthGuard } from "../httpAuth.guard";
 
@@ -12,5 +27,55 @@ export class FilesystemController {
   @Get()
   async findAll(): Promise<Filesystem> {
     return await this.filesystemService.findAll();
+  }
+
+  @Post("file")
+  async createFile(
+    @Req() request,
+    @Body() body: any
+  ): Promise<FilesystemUploadResponse> {
+    if (!request.headers["path"])
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: "You must provide a path header",
+        },
+        HttpStatus.BAD_REQUEST
+      );
+
+    let data: any;
+    switch (request.headers["content-type"]) {
+      case "application/json":
+        data = JSON.stringify(body);
+        break;
+      default:
+        data = request.readable ? await rawbody(request) : body;
+        break;
+    }
+
+    if (!data)
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: `Couldn't find data from provided Content-Type: ${request.headers["content-type"]}`,
+        },
+        HttpStatus.BAD_REQUEST
+      );
+
+    const result = await this.filesystemService.createFile(
+      request.headers["path"],
+      data
+    );
+
+    if (!result.success)
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: result.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+
+    return result;
   }
 }
