f3569d01ea773d9ddf428bcc21e4905a6d6bdd7f
Aidan Timson <contact@timmo.xyz>
2021-10-04 15:57:39 +0100

    Remove win-audio, replace with just audio from systeminformation

--- src/components/api/audio/audio.service.ts
+++ src/components/api/audio/audio.service.ts
@@ -2,51 +2,52 @@ import { Injectable } from "@nestjs/common";
 import { audio } from "systeminformation";
 
 import { Audio } from "./entities/audio.entity";
-import { getCurrent, muted, volume } from "./data";
-import { Logger } from "../../logger";
+// import { getCurrent, muted, volume } from "./data";
+// import { Logger } from "../../logger";
 import { UpdateAudioDto, UpdateAudioId } from "./dto/update-audio.dto";
 
-const { logger } = new Logger("AudioService");
+// const { logger } = new Logger("AudioService");
 
 @Injectable()
 export class AudioService {
   async findAll(): Promise<Audio> {
-    let data = {};
-    try {
-      data = { current: await getCurrent(), devices: await audio() };
-    } catch (e) {
-      logger.warn(`Cannot get audio devices: ${e.message}`);
-    }
-    return data;
+    // let data = {};
+    // try {
+    //   data = { current: await getCurrent(), devices: await audio() };
+    // } catch (e) {
+    //   logger.warn(`Cannot get audio devices: ${e.message}`);
+    // }
+    // return data;
+    return { devices: await audio() };
   }
 
   async update(
     id: UpdateAudioId,
     updateAudioDto: UpdateAudioDto
   ): Promise<Audio> {
-    const currentVolume: number = await volume();
-    switch (id) {
-      default:
-        break;
-      case "mute":
-        if (typeof updateAudioDto.value === "boolean")
-          await muted(updateAudioDto.value);
-        break;
-      case "volume":
-        if (typeof updateAudioDto.value === "number")
-          await volume(updateAudioDto.value);
-        break;
-      case "volumeDown":
-        if (typeof updateAudioDto.value === "number")
-          await volume(currentVolume - updateAudioDto.value);
-        else await volume(currentVolume - 5);
-        break;
-      case "volumeUp":
-        if (typeof updateAudioDto.value === "number")
-          await volume(currentVolume + updateAudioDto.value);
-        else await volume(currentVolume + 5);
-        break;
-    }
+    // const currentVolume: number = await volume();
+    // switch (id) {
+    //   default:
+    //     break;
+    //   case "mute":
+    //     if (typeof updateAudioDto.value === "boolean")
+    //       await muted(updateAudioDto.value);
+    //     break;
+    //   case "volume":
+    //     if (typeof updateAudioDto.value === "number")
+    //       await volume(updateAudioDto.value);
+    //     break;
+    //   case "volumeDown":
+    //     if (typeof updateAudioDto.value === "number")
+    //       await volume(currentVolume - updateAudioDto.value);
+    //     else await volume(currentVolume - 5);
+    //     break;
+    //   case "volumeUp":
+    //     if (typeof updateAudioDto.value === "number")
+    //       await volume(currentVolume + updateAudioDto.value);
+    //     else await volume(currentVolume + 5);
+    //     break;
+    // }
     return this.findAll();
   }
 }
