1ba8ab01570c3fdbd19b9ec0b80e6fd21719646b
Aidan Timson <contact@timmo.xyz>
2021-10-04 12:37:09 +0100

    Remove desktop idle

--- src/components/api/os/os.service.ts
+++ src/components/api/os/os.service.ts
@@ -2,23 +2,12 @@ import { Injectable } from "@nestjs/common";
 import { osInfo, users } from "systeminformation";
 
 import { Os } from "./entities/os.entity";
-import { Logger } from "../../logger";
-
-const { logger } = new Logger("OsService");
 
 @Injectable()
 export class OsService {
   async findAll(): Promise<Os> {
-    let idleTime = -1;
-    try {
-      const { getIdleTime } = await import("desktop-idle");
-      idleTime = getIdleTime();
-    } catch (e) {
-      logger.error(`Error ${e.message}`);
-    }
     return {
       ...(await osInfo()),
-      idleTime,
       users: await users(),
     };
   }
