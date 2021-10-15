3d84b69eaeb2121e774843c5710e325d95fb8079
Aidan Timson <contact@timmo.xyz>
2021-10-15 22:30:21 +0100

    Add max height

--- frontend/assets/jss/components/header.tsx
+++ frontend/assets/jss/components/header.tsx
@@ -10,6 +10,7 @@ const headerStyle = makeStyles((theme: Theme) => ({
     padding: "0.625rem 0",
     marginBottom: 20,
     width: "100%",
+    maxHeight: 88,
     boxShadow:
       "0 4px 18px 0px rgba(0, 0, 0, 0.12), 0 7px 10px -5px rgba(0, 0, 0, 0.15)",
     transition: "all 150ms ease 0s",
