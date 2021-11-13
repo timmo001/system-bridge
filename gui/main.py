9892f22d5401dd3dff7e099a7a2066110ea2c408
Aidan Timson <contact@timmo.xyz>
2021-11-13 22:48:54 +0000

    Close

--- gui/main.py
+++ gui/main.py
@@ -1,45 +1,122 @@
 import logging
 import sys
 from argparse import ArgumentParser, Namespace
-from PySide6.QtCore import QUrl
-from PySide6.QtGui import QIcon
+from typing import Callable
+from PySide6.QtCore import Qt, QUrl
+from PySide6.QtGui import QAction, QCloseEvent, QIcon
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QMenu, QSystemTrayIcon, QVBoxLayout, QWidget
 from urllib.parse import urlencode
 
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 FORMAT = "%(asctime)s %(levelname)s (%(threadName)s) [%(name)s] %(message)s"
 
+PATH_DATA = "/app/data"
+PATH_SETTINGS = "/app/settings"
 
-class MainWindow(QWidget):
-    def __init__(self, args: Namespace) -> None:
-        super().__init__()
 
-        logging.basicConfig(
-            format=FORMAT,
-            datefmt=DATE_FORMAT,
-            level=args.log_level.upper(),
-        )
-        logger = logging.getLogger(__name__)
+class Base:
+    def __init__(
+        self,
+        args: Namespace,
+        logger: logging.Logger,
+    ):
+        self.args = args
+        self.logger = logger
 
-        logger.debug("args: %s", args)
+
+class SystemTrayIcon(QSystemTrayIcon):
+    def __init__(
+        self,
+        icon: QIcon,
+        parent: QWidget,
+        exit: Callable[[], None],
+        show_window: Callable[[str], None],
+    ):
+        QSystemTrayIcon.__init__(self, icon, parent)
+        menu = QMenu()
+        self.show_window = show_window
+
+        action_settings: QAction = menu.addAction("Settings")
+        action_settings.triggered.connect(self.showSettings)
+
+        menu.addSeparator()
+
+        action_data: QAction = menu.addAction("View Data")
+        action_data.triggered.connect(self.showData)
+
+        menu.addSeparator()
+
+        menu.addAction("Exit", exit)
+
+        self.setContextMenu(menu)
+
+    def showData(self):
+        self.show_window(PATH_DATA)
+
+    def showSettings(self):
+        self.show_window(PATH_SETTINGS)
+
+
+class MainWindow(Base, QWidget):
+    def __init__(
+        self,
+        args: Namespace,
+        logger: logging.Logger,
+    ) -> None:
+        Base.__init__(self, args, logger)
+        QWidget.__init__(self)
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
 
         self.browser = QWebEngineView()
 
+        self.layout.addWidget(self.browser)
+
+    def setup(self, path) -> None:
         self.browser.load(
             QUrl(
-                f"""http://{args.hostname}:{args.port}/app/settings?{urlencode({
-                    "apiKey": args.api_key,
-                    "apiPort": args.port,
-                    "wsPort": args.websocket_port,
+                f"""http://{self.args.hostname}:{self.args.port}{path}?{urlencode({
+                    "apiKey": self.args.api_key,
+                    "apiPort": self.args.port,
+                    "wsPort": self.args.websocket_port,
                 })}"""
             )
         )
 
-        self.layout = QVBoxLayout(self)
-        self.layout.setContentsMargins(0, 0, 0, 0)
+    def closeEvent(self, evnt: QCloseEvent):
+        evnt.ignore()
+        self.hide()
 
-        self.layout.addWidget(self.browser)
+
+class Main(Base):
+    def __init__(
+        self,
+        args: Namespace,
+        logger: logging.Logger,
+        app: QApplication,
+    ) -> None:
+        super().__init__(args, logger)
+        self.app = app
+        self.icon = QIcon("public/system-bridge-circle.png")
+
+        self.main_window = MainWindow(self.args, self.logger)
+        self.main_window.setWindowTitle("System Bridge")
+        self.main_window.setWindowIcon(self.icon)
+        self.main_window.resize(1920, 1080)
+
+        self.systemTrayIcon = SystemTrayIcon(
+            self.icon,
+            app,
+            app.quit,
+            self.showWindow,
+        )
+        self.systemTrayIcon.show()
+
+    def showWindow(self, path: str):
+        self.logger.info(f"Showing window: {path}")
+        self.main_window.setup(path)
+        self.main_window.show()
 
 
 if __name__ == "__main__":
@@ -56,10 +133,13 @@ if __name__ == "__main__":
 
     app = QApplication([])
 
-    widget = MainWindow(args)
-    widget.setWindowTitle("System Bridge")
-    widget.setWindowIcon(QIcon("public/system-bridge-circle.png"))
-    widget.resize(1920, 1080)
-    widget.show()
+    logging.basicConfig(
+        format=FORMAT,
+        datefmt=DATE_FORMAT,
+        level=args.log_level.upper(),
+    )
+    logger = logging.getLogger(__name__)
+
+    main = Main(args, logger, app)
 
     sys.exit(app.exec())
