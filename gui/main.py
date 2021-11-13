3724966e352696059f5db1155f0e0d06186b96ae
Aidan Timson <contact@timmo.xyz>
2021-11-13 15:40:07 +0000

    Generated

---
+++ gui/main.py
@@ -0,0 +1,16 @@
+# This Python file uses the following encoding: utf-8
+import os
+from pathlib import Path
+import sys
+
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
+    if not engine.rootObjects():
+        sys.exit(-1)
+    sys.exit(app.exec_())
