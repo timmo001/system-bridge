f98019d78751e496f09ef8860fb782063b44c5d5
Aidan Timson <contact@timmo.xyz>
2021-11-13 15:43:07 +0000

    Test

--- gui/main.py
+++ gui/main.py
@@ -1,16 +1,33 @@
-# This Python file uses the following encoding: utf-8
-import os
-from pathlib import Path
 import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
 
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
 
 
 if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
-    engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
-    if not engine.rootObjects():
-        sys.exit(-1)
-    sys.exit(app.exec_())
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
--- gui/main.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick
-import QtQuick.Window
-
-Window {
-    width: 640
-    height: 480
-    visible: true
-    title: qsTr("Hello World")
-}
