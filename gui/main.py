8d0ef14f561e364ed3f40a8bc0b5f15c01e1576e
Aidan Timson <contact@timmo.xyz>
2021-11-13 18:19:03 +0000

    Logging

--- gui/main.py
+++ gui/main.py
@@ -1,22 +1,28 @@
 import logging
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from PySide6.QtCore import QUrl
 from PySide6.QtGui import QIcon
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
 from urllib.parse import urlencode
 
-_LOGGER = logging.getLogger(__name__)
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+FORMAT = "%(asctime)s %(levelname)s (%(threadName)s) [%(name)s] %(message)s"
 
 
-class MyWidget(QWidget):
-    def __init__(self, args) -> None:
+class MainWindow(QWidget):
+    def __init__(self, args: Namespace) -> None:
         super().__init__()
 
-        _LOGGER.setLevel(args.log_level.upper())
+        logging.basicConfig(
+            format=FORMAT,
+            datefmt=DATE_FORMAT,
+            level=args.log_level.upper(),
+        )
+        logger = logging.getLogger(__name__)
 
-        _LOGGER.debug("args: %s", args)
+        logger.debug("args: %s", args)
 
         self.browser = QWebEngineView()
 
@@ -50,7 +56,7 @@ if __name__ == "__main__":
 
     app = QApplication([])
 
-    widget = MyWidget(args)
+    widget = MainWindow(args)
     widget.setWindowTitle("System Bridge")
     widget.setWindowIcon(QIcon("public/system-bridge-circle.png"))
     widget.resize(1280, 720)
