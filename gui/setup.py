0a5029f4bad172f723d4f956f6abda01cbde225d
Aidan Timson <contact@timmo.xyz>
2021-11-14 21:13:26 +0000

    List dirs (test)

--- gui/setup.py
+++ gui/setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 import io
+import os
 import json
 
 from setuptools import find_packages, setup
 
+print(os.listdir("./"))
+
 # Get packages from requirements.txt
 with io.open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
+
+print(os.listdir("../"))
 
 # Get version from package.json
 with io.open("../package.json", encoding="utf-8") as f:
