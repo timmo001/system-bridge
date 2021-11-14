2eaa4bb179aa84b130566499671d1d86f2db0cab
Aidan Timson <contact@timmo.xyz>
2021-11-14 17:32:19 +0000

    Pylint fixes

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,16 +1,17 @@
+"""System Bridge GUI"""
+from argparse import ArgumentParser
 import asyncio
+from collections.abc import Callable
 import logging
 import sys
-from argparse import ArgumentParser, Namespace
-from typing import Callable
 from urllib.parse import urlencode
 from webbrowser import open_new_tab
 
-from aiohttp.client import ClientSession
 from PySide6.QtCore import QUrl
 from PySide6.QtGui import QAction, QCloseEvent, QIcon
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWidgets import QApplication, QMenu, QSystemTrayIcon, QVBoxLayout, QWidget
+from aiohttp.client import ClientSession
 from systembridge import Bridge, BridgeClient
 from systembridge.objects.information import Information
 
@@ -27,45 +28,29 @@ URL_ISSUES = "https://github.com/timmo001/system-bridge/issues/new/choose"
 URL_LATEST_RELEASE = "https://github.com/timmo001/system-bridge/releases/latest"
 
 
-class Base:
-    """Base class"""
-
-    def __init__(
-        self,
-        args: Namespace,
-        logger: logging.Logger,
-    ):
-        """Initialize the base class"""
-        self.args = args
-        self.logger = logger
-
-
-class SystemTrayIcon(Base, QSystemTrayIcon):
+class SystemTrayIcon(QSystemTrayIcon):
     """System Tray Icon"""
 
     def __init__(
         self,
-        args: Namespace,
-        logger: logging.Logger,
         icon: QIcon,
         parent: QWidget,
         information: Information,
-        exit: Callable[[], None],
-        show_window: Callable[[str], None],
-    ):
+        callback_exit_application: Callable[[], None],
+        callback_show_window: Callable[[str], None],
+    ) -> None:
         """Initialize the system tray icon"""
-        Base.__init__(self, args, logger)
         QSystemTrayIcon.__init__(self, icon, parent)
         menu = QMenu()
-        self.show_window = show_window
+        self.callback_show_window = callback_show_window
 
         action_settings: QAction = menu.addAction("Open Settings")
-        action_settings.triggered.connect(self.showSettings)
+        action_settings.triggered.connect(self.show_settings)
 
         menu.addSeparator()
 
         action_data: QAction = menu.addAction("View Data")
-        action_data.triggered.connect(self.showData)
+        action_data.triggered.connect(self.show_data)
 
         menu.addSeparator()
 
@@ -99,76 +84,79 @@ class SystemTrayIcon(Base, QSystemTrayIcon):
                     })"""
 
         action_latest_release: QAction = menu.addAction(latest_version_text)
-        action_latest_release.triggered.connect(self.openLatestReleases)
+        action_latest_release.triggered.connect(self.open_latest_releases)
 
         menu_help = menu.addMenu("Help")
 
         action_docs: QAction = menu_help.addAction("Documentation / Website")
-        action_docs.triggered.connect(self.openDocs)
+        action_docs.triggered.connect(self.open_docs)
 
         action_feature: QAction = menu_help.addAction("Suggest a Feature")
-        action_feature.triggered.connect(self.openFeatureRequest)
+        action_feature.triggered.connect(self.open_feature_request)
 
         action_issue: QAction = menu_help.addAction("Report an issue")
-        action_issue.triggered.connect(self.openIssues)
+        action_issue.triggered.connect(self.open_issues)
 
         action_discussions: QAction = menu_help.addAction("Discussions")
-        action_discussions.triggered.connect(self.openDiscussions)
+        action_discussions.triggered.connect(self.open_discussions)
 
         menu_help.addSeparator()
 
         action_logs: QAction = menu_help.addAction("View Logs")
-        action_logs.triggered.connect(self.showLogs)
+        action_logs.triggered.connect(self.show_logs)
 
         menu.addSeparator()
 
         action_exit: QAction = menu.addAction("Exit")
-        action_exit.triggered.connect(exit)
+        action_exit.triggered.connect(callback_exit_application)
 
         self.setContextMenu(menu)
 
-    def showData(self):
-        """Show api data"""
-        self.show_window(PATH_DATA)
-
-    def openLatestReleases(self):
+    @staticmethod
+    def open_latest_releases() -> None:
         """Open latest release"""
         open_new_tab(URL_LATEST_RELEASE)
 
-    def showLogs(self):
-        """Show logs"""
-        self.show_window(PATH_LOGS)
-
-    def showSettings(self):
-        """Show settings"""
-        self.show_window(PATH_SETTINGS)
-
-    def openDocs(self):
+    @staticmethod
+    def open_docs() -> None:
         """Open documentation"""
         open_new_tab(URL_DOCS)
 
-    def openFeatureRequest(self):
+    @staticmethod
+    def open_feature_request() -> None:
         """Open feature request"""
         open_new_tab(URL_ISSUES)
 
-    def openIssues(self):
+    @staticmethod
+    def open_issues() -> None:
         """Open issues"""
         open_new_tab(URL_ISSUES)
 
-    def openDiscussions(self):
+    @staticmethod
+    def open_discussions() -> None:
         """Open discussions"""
         open_new_tab(URL_DISCUSSIONS)
 
+    def show_data(self) -> None:
+        """Show api data"""
+        self.callback_show_window(PATH_DATA)
 
-class MainWindow(Base, QWidget):
-    def __init__(
-        self,
-        args: Namespace,
-        logger: logging.Logger,
-    ) -> None:
+    def show_logs(self) -> None:
+        """Show logs"""
+        self.callback_show_window(PATH_LOGS)
+
+    def show_settings(self) -> None:
+        """Show settings"""
+        self.callback_show_window(PATH_SETTINGS)
+
+
+class MainWindow(QWidget):
+    """Main Window"""
+
+    def __init__(self) -> None:
         """Initialize the main window"""
-        Base.__init__(self, args, logger)
         QWidget.__init__(self)
+
         self.layout = QVBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
 
@@ -176,6 +164,7 @@ class MainWindow(Base, QWidget):
 
         self.layout.addWidget(self.browser)
 
+    # pylint: disable=invalid-name
     def closeEvent(self, event: QCloseEvent):
         """Close the window instead of closing the app"""
         event.ignore()
@@ -184,79 +173,74 @@ class MainWindow(Base, QWidget):
     def setup(self, path) -> None:
         """Setup the main window"""
         url = QUrl(
-            f"""http://{self.args.hostname}:{self.args.frontend_port}{path}?{urlencode({
-                    "apiKey": self.args.api_key,
-                    "apiPort": self.args.port,
-                    "wsPort": self.args.websocket_port,
+            f"""http://{args.hostname}:{args.frontend_port}{path}?{urlencode({
+                    "apiKey": args.api_key,
+                    "apiPort": args.port,
+                    "wsPort": args.websocket_port,
                 })}"""
         )
-        self.logger.debug(f"Opening url: {url}")
+        logger.debug("Opening url: %s", url)
         self.browser.load(url)
 
 
-class Main(Base):
+class Main:
     """Main class"""
 
-    def __init__(
-        self,
-        args: Namespace,
-        logger: logging.Logger,
-        app: QApplication,
-    ) -> None:
+    def __init__(self) -> None:
         """Initialize the main class"""
-        super().__init__(args, logger)
-        self.app = app
         self.icon = QIcon("public/system-bridge-circle.png")
-        asyncio.run(self.setupBridge())
+        asyncio.run(self.setup_bridge())
 
-        self.main_window = MainWindow(self.args, self.logger)
+        self.main_window = MainWindow()
         self.main_window.setWindowTitle("System Bridge")
         self.main_window.setWindowIcon(self.icon)
         self.main_window.resize(1280, 720)
 
-        self.systemTrayIcon = SystemTrayIcon(
-            self.args,
-            self.logger,
+        self.system_tray_icon = SystemTrayIcon(
             self.icon,
-            self.app,
+            application,
             self.information,
-            self.exitApplication,
-            self.showWindow,
+            self.callback_exit_application,
+            self.callback_show_window,
         )
-        self.systemTrayIcon.show()
+        self.system_tray_icon.show()
 
-    def exitApplication(self) -> None:
+    def callback_exit_application(self) -> None:
         """Exit the application"""
-        self.logger.info("Exiting application..")
-        asyncio.run(self.exitBackend())
-        self.app.quit()
+        logger.info("Exiting application..")
+        asyncio.run(self.exit_backend())
+        application.quit()
 
-    async def exitBackend(self) -> None:
-        """Exit the backend"""
-        await self.bridge.async_connect_websocket(
-            self.args.hostname,
-            self.args.websocket_port,
-        )
-        await self.bridge.async_send_event("exit-application", {})
-
-    async def setupBridge(self) -> None:
-        """Setup the bridge"""
-        async with ClientSession() as session:
-            self.bridge = Bridge(
-                BridgeClient(session),
-                f"http://{self.args.hostname}:{self.args.port}",
-                self.args.api_key,
-            )
-            self.information = await self.bridge.async_get_information()
-
-    def showWindow(self, path: str, maximized: bool = True) -> None:
+    def callback_show_window(
+        self,
+        path: str,
+        maximized: bool = True,
+    ) -> None:
         """Show the main window"""
-        self.logger.info(f"Showing window: {path}")
+        logger.info("Showing window: %s", path)
         self.main_window.setup(path)
         if maximized:
             self.main_window.showMaximized()
         else:
             self.main_window.show()
+
+    async def exit_backend(self) -> None:
+        """Exit the backend"""
+        await self.bridge.async_connect_websocket(
+            args.hostname,
+            args.websocket_port,
+        )
+        await self.bridge.async_send_event("exit-application", {})
+
+    async def setup_bridge(self) -> None:
+        """Setup the bridge"""
+        async with ClientSession() as session:
+            self.bridge = Bridge(
+                BridgeClient(session),
+                f"http://{args.hostname}:{args.port}",
+                args.api_key,
+            )
+            self.information = await self.bridge.async_get_information()
 
 
 if __name__ == "__main__":
@@ -305,7 +289,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    app = QApplication([])
+    application = QApplication([])
 
     logging.basicConfig(
         format=FORMAT,
@@ -314,6 +298,6 @@ if __name__ == "__main__":
     )
     logger = logging.getLogger(__name__)
 
-    main = Main(args, logger, app)
+    Main()
 
-    sys.exit(app.exec())
+    sys.exit(application.exec())
