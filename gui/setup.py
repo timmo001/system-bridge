d1536aa2f1916bd54c4b2716cea6e377c9ac0749
Aidan Timson <contact@timmo.xyz>
2021-11-14 21:35:55 +0000

    Fix

--- gui/setup.py
+++ gui/setup.py
@@ -13,16 +13,18 @@ def find(name, path):
             return os.path.join(root, name)
 
 
-print(find("package.json", "./"))
-print(find("package.json", "../"))
+# print(find("package.json", "./"))
+# print(find("package.json", "../"))
 
 # Get packages from requirements.txt
 with io.open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 # Get version from package.json
+print("./")
 path = find("package.json", "./")
 if path is None:
+    print("../")
     path = find("package.json", "../")
 
 print(path)
@@ -34,15 +36,15 @@ version = package["version"]
 
 print(version)
 
-setup(
-    name="systembridgegui",
-    version=version,
-    description="System Bridge GUI",
-    keywords="system bridge gui",
-    author="Aidan Timson (Timmo)",
-    author_email="contact@timmo.xyz",
-    license="MIT",
-    url="https://github.com/timmo001/system-bridge",
-    packages=find_packages(exclude=["tests", "generator"]),
-    install_requires=requirements,
-)
+# setup(
+#     name="systembridgegui",
+#     version=version,
+#     description="System Bridge GUI",
+#     keywords="system bridge gui",
+#     author="Aidan Timson (Timmo)",
+#     author_email="contact@timmo.xyz",
+#     license="MIT",
+#     url="https://github.com/timmo001/system-bridge",
+#     packages=find_packages(exclude=["tests", "generator"]),
+#     install_requires=requirements,
+# )
