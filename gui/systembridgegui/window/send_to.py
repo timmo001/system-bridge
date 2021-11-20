510442e8d52a862aab7e8f0a1a78ea05ee85dfae
Aidan Timson <contact@timmo.xyz>
2021-11-20 03:24:22 +0000

    Inputs

--- gui/systembridgegui/window/send_to.py
+++ gui/systembridgegui/window/send_to.py
@@ -3,7 +3,14 @@ from argparse import Namespace
 from typing import List
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QCloseEvent, QIcon
-from PySide6.QtWidgets import QComboBox, QLabel, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtWidgets import (
+    QComboBox,
+    QLabel,
+    QPushButton,
+    QTextEdit,
+    QVBoxLayout,
+    QWidget,
+)
 from zeroconf.asyncio import AsyncServiceInfo
 
 from ..base import Base
@@ -38,15 +45,25 @@ class SendToWindow(Base, QWidget):
         label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
 
         self.combo_box = QComboBox()
-        # self.combo_box.currentIndexChanged.connect(
-        #     lambda index: self.logger.info("Index changed: %s", index)
-        # )
+        self.combo_box.currentIndexChanged.connect(lambda index: self.set_data(index))
+
+        self.text_host = QTextEdit()
+        self.text_host.setPlaceholderText("Host")
+
+        self.text_port = QTextEdit(self.args.port)
+        self.text_port.setPlaceholderText("Port")
+
+        self.text_api_key = QTextEdit(self.args.api_key)
+        self.text_api_key.setPlaceholderText("API Key")
 
         button = QPushButton("Send")
         button.clicked.connect(self.send)
 
         self.layout.addWidget(label, 1, Qt.AlignTop | Qt.AlignVCenter)
         self.layout.addWidget(self.combo_box, 1, Qt.AlignHCenter | Qt.AlignVCenter)
+        self.layout.addWidget(self.text_host, 1, Qt.AlignHCenter | Qt.AlignVCenter)
+        self.layout.addWidget(self.text_port, 1, Qt.AlignHCenter | Qt.AlignVCenter)
+        self.layout.addWidget(self.text_api_key, 1, Qt.AlignHCenter | Qt.AlignVCenter)
         self.layout.addWidget(button, 1, Qt.AlignBottom | Qt.AlignVCenter)
 
         get_or_create_event_loop()
@@ -65,6 +82,7 @@ class SendToWindow(Base, QWidget):
     def setup(self) -> None:
         """Setup the window"""
         self.combo_box.clear()
+
         for service in self.services:
             host = service.properties.get(b"host")
             ip = service.properties.get(b"ip")
@@ -78,6 +96,29 @@ class SendToWindow(Base, QWidget):
         self.combo_box.addItem("Manual")
 
         self.combo_box.setCurrentIndex(0)
+        self.set_data(0)
+
+    def set_data(self, index: int) -> None:
+        """Set data"""
+        if index < 0:
+            return
+
+        if index == self.combo_box.count() - 1:
+            self.text_host.setEnabled(True)
+            self.text_port.setEnabled(True)
+            self.text_api_key.setEnabled(True)
+            self.text_host.setText("")
+            self.text_port.setText("")
+        else:
+            service = self.services[index]
+
+            self.text_host.setEnabled(False)
+            self.text_port.setEnabled(False)
+            self.text_api_key.setEnabled(False)
+            self.text_host.setText(service.properties.get(b"host"))
+            self.text_port.setText(service.properties.get(b"port"))
+
+        self.text_api_key.setText("")
 
     def send(self) -> None:
         """Send the selected service"""
