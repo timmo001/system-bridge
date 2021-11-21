74d6937be6e1c48aa43fc6d606510bc8059b1cdf
Aidan Timson <contact@timmo.xyz>
2021-11-21 20:29:46 +0000

    Test

--- gui/systembridgegui/main.py
+++ gui/systembridgegui/main.py
@@ -80,6 +80,7 @@ class Main(Base):
             self.application,
             self.icon,
             True,
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
         )
         self.player_window.show()
 
