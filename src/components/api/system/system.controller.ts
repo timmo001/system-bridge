4190c2a5aa9e2a32691dd06f5b25efb0fdd2c532
Aidan Timson <contact@timmo.xyz>
2021-11-26 17:55:48 +0000

    Split system endpoint

--- src/components/api/system/system.controller.ts
+++ src/components/api/system/system.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Systeminformation } from "systeminformation";
 
 import { HttpAuthGuard } from "../httpAuth.guard";
 import { System } from "./entities/system.entity";
@@ -12,5 +13,30 @@ export class SystemController {
   @Get()
   async findAll(): Promise<System> {
     return await this.systemService.findAll();
+  }
+
+  @Get("baseboard")
+  async findBaseboard(): Promise<Systeminformation.BaseboardData> {
+    return await this.systemService.findBaseboard();
+  }
+
+  @Get("bios")
+  async findBios(): Promise<Systeminformation.BiosData> {
+    return await this.systemService.findBios();
+  }
+
+  @Get("chassis")
+  async findChassis(): Promise<Systeminformation.ChassisData> {
+    return await this.systemService.findChassis();
+  }
+
+  @Get("system")
+  async findSystem(): Promise<Systeminformation.SystemData> {
+    return await this.systemService.findSystem();
+  }
+
+  @Get("uuid")
+  async findUuid(): Promise<Systeminformation.UuidData> {
+    return this.systemService.findUuid();
   }
 }
