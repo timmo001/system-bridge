d520ee713d4d38e280082f862b0a54e918668de4
Aidan Timson <contact@timmo.xyz>
2021-11-26 22:50:49 +0000

    Cleanup

--- src/components/api/media/media.service.ts
+++ src/components/api/media/media.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { tmpdir } from "os";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import fs from "fs";
@@ -7,8 +8,10 @@ import { AudioSource, Media, VideoSource } from "./entities/media.entity";
 import { CreateMediaDto } from "./dto/create-media.dto";
 import { DeleteMediaDto } from "./dto/delete-media.dto";
 import { FindMediaId } from "./dto/find-media.dto";
+import { getConnection, getSettingsObject } from "../..//common";
 import { Logger } from "src/components/logger";
 import { UpdateMediaDto, UpdateMediaId } from "./dto/update-media.dto";
+import { WebSocketConnection } from "../../websocket";
 
 @Injectable()
 export class MediaService {
@@ -68,34 +71,48 @@ export class MediaService {
   //   }
   // }
 
-  async create(createMediaDto: CreateMediaDto): Promise<CreateMediaDto> {
+  create(createMediaDto: CreateMediaDto): CreateMediaDto {
     (async () => {
-      // closePlayerWindow();
-      if (createMediaDto.type === "audio")
-        if (createMediaDto.path) {
-          // setSetting("current-media-path", createMediaDto.path);
-        } else if (createMediaDto.url) {
-          // createMediaDto.path = app.getPath("temp") + `/media-${uuidv4()}`;
-          const { logger } = new Logger("MediaService");
-          logger.info(`Downloading: ${createMediaDto.url}`);
-          const response = await axios.get(createMediaDto.url, {
-            responseType: "stream",
-          });
-          const writer = fs.createWriteStream(createMediaDto.path);
-          response.data.pipe(writer);
-          await new Promise((resolve, reject) => {
-            writer.on("finish", resolve);
-            writer.on("error", reject);
-          });
+      const connection = await getConnection();
+      const settings = await getSettingsObject(connection);
+      await connection.close();
+
+      const ws = new WebSocketConnection(
+        Number(settings["network-wsPort"]) || 9172,
+        settings["network-apiKey"],
+        false,
+        async () => {
+          ws.sendEvent({ name: "gui-player-close" });
+          if (createMediaDto.type === "audio")
+            if (createMediaDto.path) {
+              // setSetting("current-media-path", createMediaDto.path);
+            } else if (createMediaDto.url) {
+              createMediaDto.path = tmpdir() + `/media-${uuidv4()}`;
+              const { logger } = new Logger("MediaService");
+              logger.info(`Downloading: ${createMediaDto.url}`);
+              const response = await axios.get(createMediaDto.url, {
+                responseType: "stream",
+              });
+              const writer = fs.createWriteStream(createMediaDto.path);
+              response.data.pipe(writer);
+              await new Promise((resolve, reject) => {
+                writer.on("finish", resolve);
+                writer.on("error", reject);
+              });
+            }
+          if (createMediaDto.path) {
+            ws.sendEvent({
+              name: "gui-player-create",
+              data: {
+                ...createMediaDto,
+                url: `file://${createMediaDto.path}`,
+              },
+            });
+          } else if (createMediaDto.url) {
+            ws.sendEvent({ name: "gui-player-create", data: createMediaDto });
+          }
         }
-      if (createMediaDto.path) {
-        // createPlayerWindow({
-        //   ...createMediaDto,
-        //   url: `safe-file-protocol://${createMediaDto.path}`,
-        // });
-      } else if (createMediaDto.url) {
-        // createPlayerWindow(createMediaDto);
-      }
+      );
     })();
     return createMediaDto;
   }
