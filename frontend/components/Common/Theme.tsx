3474eab62a1ca6dc3544643f88b8cdb507d8c2b6
Aidan Timson <contact@timmo.xyz>
2022-02-21 10:00:53 +0000

    Change MUI v4 to v5 theme (#1362)
    * Change MUI v4 to v5 theme
    
    * Prettified Code!
    
    Co-authored-by: timmo001 <timmo001@users.noreply.github.com>

--- frontend/components/Common/Theme.tsx
+++ frontend/components/Common/Theme.tsx
@@ -1,34 +1,32 @@
-import {
-  createTheme,
-  responsiveFontSizes,
-  adaptV4Theme,
-} from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import { deepPurple, grey } from "@mui/material/colors";
 
-let theme = createTheme(
-  adaptV4Theme({
-    palette: {
-      mode: "dark",
-      primary: {
-        dark: deepPurple[800],
-        main: deepPurple[700],
-        light: deepPurple[600],
-      },
-      secondary: deepPurple,
-      background: {
-        default: grey[900],
-        paper: "#292929",
-      },
-      contrastThreshold: 3,
-      tonalOffset: 0.2,
+let theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      dark: deepPurple[800],
+      main: deepPurple[700],
+      light: deepPurple[600],
     },
-    overrides: {
-      MuiCard: {
+    secondary: deepPurple,
+    background: {
+      default: grey[900],
+      paper: "#292929",
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  components: {
+    MuiCard: {
+      styleOverrides: {
         root: {
           margin: 8,
         },
       },
-      MuiCardContent: {
+    },
+    MuiCardContent: {
+      styleOverrides: {
         root: {
           padding: "24px 32px",
           "&:last-child": {
@@ -36,19 +34,23 @@ let theme = createTheme(
           },
         },
       },
-      MuiChip: {
+    },
+    MuiChip: {
+      styleOverrides: {
         root: {
           margin: 4,
         },
       },
-      MuiCardActions: {
+    },
+    MuiCardActions: {
+      styleOverrides: {
         root: {
           justifyContent: "flex-end",
         },
       },
     },
-  })
-);
+  },
+});
 theme = responsiveFontSizes(theme);
 
 export default theme;
