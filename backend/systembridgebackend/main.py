d29301224bc24830d3b1673339e750a857b0ed4f
Aidan Timson <contact@timmo.xyz>
2022-04-17 17:38:39 +0100

    Cleanup logs

--- backend/systembridgebackend/main.py
+++ backend/systembridgebackend/main.py
@@ -12,22 +12,17 @@ class Main(Base):
     def __init__(self) -> None:
         """Initialize the main class"""
         super().__init__()
+        self._logger.info("System Bridge")
 
         self._database = Database()
-
-        self._logger.info("----------------------------------------------------")
-        self._logger.info("System Bridge")
-        self._logger.info("----------------------------------------------------")
 
         asyncio.run(self.setup())
 
     async def setup(self) -> None:
         """Setup application"""
-        self._logger.info("Setup application")
         if not self._database.connected:
             self._database.connect()
 
-        update_cpu = CPUUpdate(self._database)
-        await update_cpu.update_all_data()
+        await CPUUpdate(self._database).update_all_data()
 
         self._logger.info(self._database.read_table("cpu").to_json(orient="records"))
