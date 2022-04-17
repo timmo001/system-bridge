f3031847f478ceff75551af7a9a4d720f3ea9611
Aidan Timson <contact@timmo.xyz>
2022-04-17 20:48:31 +0100

    Generic handlers

--- backend/systembridgebackend/server/__init__.py
+++ backend/systembridgebackend/server/__init__.py
@@ -1,9 +1,10 @@
 """System Bridge: Server"""
-from sanic import Sanic
 from sqlite3 import Connection
+from sanic import Sanic
+from sanic.request import Request
+from sanic.response import HTTPResponse, json
 
 from systembridgebackend import Base
-from systembridgebackend.server.cpu import CPU
 
 
 class ServerBase(Base):
@@ -29,7 +30,31 @@ class Server(ServerBase):
         super().__init__(database)
         self._server = Sanic("SystemBridge")
 
-        CPU(self._database, self._server)
+        async def handler_all(
+            request: Request,
+            table: str,
+        ) -> HTTPResponse:
+            return json(self._database.table_data_to_ordered_dict(table))
+
+        async def handler_key(
+            request: Request,
+            table: str,
+            key: str,
+        ) -> HTTPResponse:
+            data = self._database.read_table_by_key(table, key).to_dict(
+                orient="records"
+            )[0]
+            return json(
+                {
+                    data["key"]: data["value"],
+                    "last_updated": data["timestamp"],
+                }
+            )
+
+        self._server.add_route(handler_all, "/api/<table:str>", methods=["GET"])
+        self._server.add_route(
+            handler_key, "/api/<table:str>/<key:str>", methods=["GET"]
+        )
 
         # self._server.static("/", "./frontend/dist/")
         # self._server.add_websocket_route(websocket, "/api/websocket")
