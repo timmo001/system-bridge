f9daf023a89138dbe980138b77edae17aa2fd028
Aidan Timson <contact@timmo.xyz>
2022-04-18 14:27:22 +0100

    Move version to package dir

--- backend/setup.py
+++ backend/setup.py
@@ -1,29 +1,14 @@
 """Setup"""
 import io
-import os
 
 from setuptools import find_packages, setup
-
-
-def find(name: str, path: str) -> str:
-    """Find a file in a directory."""
-    for root, _, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
-    return None
-
 
 # Get packages from requirements.txt
 with io.open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 # Get version from version.txt
-version_path = find("version.txt", "../")
-print("Path:", version_path)
-if not version_path:
-    version_path = find("version.txt", "./")
-print("Path:", version_path)
-with io.open(version_path, encoding="utf-8") as f:
+with io.open("version.txt", encoding="utf-8") as f:
     version = f.read().splitlines()[0]
 
 setup(
