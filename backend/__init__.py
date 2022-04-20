57e27ff6a87f33aeb7c2e44e31f1ba448485d846
Aidan Timson <contact@timmo.xyz>
2022-04-20 15:01:48 +0100

    Logging to user data directory

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,7 @@
 """System Bridge"""
 import logging
+import os
+from appdirs import AppDirs
 
 from systembridgebackend.main import Main
 
@@ -7,11 +9,18 @@ DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 FORMAT = "%(asctime)s %(levelname)s (%(threadName)s) [%(name)s] %(message)s"
 
 if __name__ == "__main__":
+    user_data_dir = AppDirs("systembridge", "timmo001").user_data_dir
+
+    # Create User Data Directories
+    os.makedirs(user_data_dir, exist_ok=True)
+
     logging.basicConfig(
-        format=FORMAT,
         datefmt=DATE_FORMAT,
-        level="INFO",
+        format=FORMAT,
+        handlers=[
+            logging.FileHandler(os.path.join(user_data_dir, "system-bridge.log")),
+        ],
+        level=logging.INFO,
     )
-    logger = logging.getLogger(__name__)
 
     Main()
