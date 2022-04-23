975656c443ba32724235e36ea5f837a93ab5be18
Aidan Timson <contact@timmo.xyz>
2022-04-23 18:15:20 +0100

    Add missing dependency

--- gui/systembridgegui/__main__.py
+++ gui/systembridgegui/__main__.py
@@ -4,6 +4,7 @@ import sys
 
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QMessageBox
+
 from systembridgeshared.base import Base
 from systembridgeshared.const import SETTING_LOG_LEVEL
 from systembridgeshared.database import Database
@@ -11,7 +12,6 @@ from systembridgeshared.exceptions import ConnectionErrorException
 from systembridgeshared.logger import setup_logger
 from systembridgeshared.settings import Settings
 from systembridgeshared.websocket_client import WebSocketClient
-
 from systembridgegui.system_tray import SystemTray
 from systembridgegui.window.main import MainWindow
 
