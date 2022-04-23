32f83fd7eabea90f0520218954d57bc8a11758e2
Aidan Timson <contact@timmo.xyz>
2022-04-23 17:14:12 +0100

    Include module in logger name

--- shared/systembridgeshared/base.py
+++ shared/systembridgeshared/base.py
@@ -7,5 +7,6 @@ class Base:  # pylint: disable=too-few-public-methods
 
     def __init__(self):
         """Initialize"""
-        self._logger = logging.getLogger(self.__class__.__name__)
-        self._logger.debug("%s __init__", self.__class__.__name__)
+        name = f"{self.__class__.__module__}.{self.__class__.__name__}"
+        self._logger = logging.getLogger(name)
+        self._logger.debug("%s __init__", name)
