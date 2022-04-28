de428c16c3846083b2afba553e14b22e09c29f95
Aidan Timson <contact@timmo.xyz>
2022-04-28 10:29:25 +0100

    add version

---
+++ cli/systembridgecli/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides systembridgecli version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update systembridgecli` to change this file.
+
+from incremental import Version
+
+__version__ = Version("systembridgecli", 3, 0, 0, dev=0)
+__all__ = ["__version__"]
