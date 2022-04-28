131e33200f31698e0880b6e541f970c199721972
Aidan Timson <contact@timmo.xyz>
2022-04-28 10:18:07 +0100

    Incremental package

--- connector/setup.py
+++ connector/setup.py
@@ -1,22 +1,20 @@
 """Setup"""
 import io
-import os
 
 from setuptools import find_packages, setup
+
+PACKAGE_NAME = "systembridgeconnector"
+
+# Get setup packages from requirements.txt
+with io.open("requirements_setup.txt", encoding="utf-8") as f:
+    requirements_setup = f.read().splitlines()
 
 # Get packages from requirements.txt
 with io.open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
-# Get version from version.txt
-with io.open(
-    os.path.join(os.path.dirname(__file__), "../version.txt"), encoding="utf-8"
-) as f:
-    version = f.read().splitlines()[0]
-
 setup(
-    name="systembridgeconnector",
-    version=version,
+    name=PACKAGE_NAME,
     description="System Bridge Connector",
     keywords="system bridge connector",
     author="Aidan Timson (Timmo)",
@@ -25,4 +23,6 @@ setup(
     url="https://github.com/timmo001/system-bridge",
     packages=find_packages(exclude=["tests", "generator"]),
     install_requires=requirements,
+    setup_requires=requirements_setup,
+    use_incremental=True,
 )
