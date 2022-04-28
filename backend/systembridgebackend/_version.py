137c28316b690c925b515836c0858089cf21b476
Aidan Timson <contact@timmo.xyz>
2022-04-28 10:54:57 +0100

    Add version

---
+++ backend/systembridgebackend/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides systembridgebackend version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update systembridgebackend` to change this file.
+
+from incremental import Version
+
+__version__ = Version("systembridgebackend", 3, 0, 0, dev=0)
+__all__ = ["__version__"]
