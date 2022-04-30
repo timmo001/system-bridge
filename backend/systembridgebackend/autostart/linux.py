324fb190c9e7af6739b186de64d984c43e017e1c
Aidan Timson <contact@timmo.xyz>
2022-04-30 23:32:33 +0100

    TODO

--- backend/systembridgebackend/autostart/linux.py
+++ backend/systembridgebackend/autostart/linux.py
@@ -9,5 +9,7 @@ def autostart_linux_disable():
 
 def autostart_linux_enable():
     """Enable autostart for Linux"""
+    # TODO: https://www.google.co.uk/search?q=add+to+startup+python+script+linux
+    # https://stackoverflow.com/questions/24518522/run-python-script-at-startup-in-ubuntu
     # address = f"{sys.executable} -m systembridgebackend"
     raise Exception("Method not implemented")
