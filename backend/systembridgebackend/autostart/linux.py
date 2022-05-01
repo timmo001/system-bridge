b155d5b1ab237a4260e56d838b86bb32cc8bc768
Aidan Timson <contact@timmo.xyz>
2022-05-01 14:12:47 +0100

    Create linux desktop entries

--- backend/systembridgebackend/autostart/linux.py
+++ backend/systembridgebackend/autostart/linux.py
@@ -1,15 +1,29 @@
 """System Bridge: Autostart Linux"""
+import os
+import sys
+
+desktop_entry = f"""
+[Desktop Entry]
+Name=System Bridge
+Comment=System Bridge
+Exec={sys.executable} -m systembridgebackend
+Icon={os.path.join(os.path.dirname(__file__),'../icon.png')}
+Terminal=false
+Type=Application
+Categories=Application;
+"""
+
+path = os.path.expanduser("~/.config/autostart/systembridge.desktop")
 
 
 def autostart_linux_disable():
     """Disable autostart for Linux"""
-    # address = f"{sys.executable} -m systembridgebackend"
-    raise Exception("Method not implemented")
+    if os.path.exists(path):
+        os.remove(path)
 
 
 def autostart_linux_enable():
     """Enable autostart for Linux"""
-    # TODO: https://www.google.co.uk/search?q=add+to+startup+python+script+linux
-    # https://stackoverflow.com/questions/24518522/run-python-script-at-startup-in-ubuntu
-    # address = f"{sys.executable} -m systembridgebackend"
-    raise Exception("Method not implemented")
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            f.write(desktop_entry)
