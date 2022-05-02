aa2bb88708dfc073a3d7de3ee9091064826e5c89
timmo001 <timmo001@users.noreply.github.com>
2022-05-02 02:48:57 +0000

    Bump systembridgebackend version to 3.0.0.dev26

--- backend/systembridgebackend/_version.py
+++ backend/systembridgebackend/_version.py
@@ -7,5 +7,5 @@ Provides systembridgebackend version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgebackend", 3, 0, 0, dev=25)
+__version__ = Version("systembridgebackend", 3, 0, 0, dev=26)
 __all__ = ["__version__"]
