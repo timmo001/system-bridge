63585025726d17e028e333d2b96f97d59e2124ce
Aidan Timson <contact@timmo.xyz>
2022-05-02 19:13:40 +0100

    Update versions to 3.0.0

--- connector/systembridgeconnector/_version.py
+++ connector/systembridgeconnector/_version.py
@@ -7,5 +7,5 @@ Provides systembridgeconnector version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgeconnector", 3, 0, 0, dev=34)
+__version__ = Version("systembridgeconnector", 3, 0, 0)
 __all__ = ["__version__"]
