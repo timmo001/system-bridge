901fb20805bc500658af8e51ef22f4876ee6670f
Aidan Timson <contact@timmo.xyz>
2022-05-05 16:20:46 +0100

    Dev versions

--- windowssensors/systembridgewindowssensors/_version.py
+++ windowssensors/systembridgewindowssensors/_version.py
@@ -7,5 +7,5 @@ Provides systembridgewindowssensors version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgewindowssensors", 3, 0, 3)
+__version__ = Version("systembridgewindowssensors", 3, 0, 3, dev=0)
 __all__ = ["__version__"]
