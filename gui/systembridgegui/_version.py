2fcfe0270b4360ab4027a3f60d75f92ee1f3dec8
timmo001 <timmo001@users.noreply.github.com>
2022-05-16 17:29:53 +0000

    Bump systembridgegui version to 3.0.7.dev3

--- gui/systembridgegui/_version.py
+++ gui/systembridgegui/_version.py
@@ -7,5 +7,5 @@ Provides systembridgegui version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgegui", 3, 0, 7, dev=2)
+__version__ = Version("systembridgegui", 3, 0, 7, dev=3)
 __all__ = ["__version__"]
