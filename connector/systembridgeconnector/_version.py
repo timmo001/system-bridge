f6e053dc70a56bde8e5cdfa39fa4ee446d20dc73
Aidan Timson <contact@timmo.xyz>
2022-05-16 18:54:28 +0100

    Bump systembridgeconnector version to 3.0.8.dev0

--- connector/systembridgeconnector/_version.py
+++ connector/systembridgeconnector/_version.py
@@ -7,5 +7,5 @@ Provides systembridgeconnector version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgeconnector", 3, 0, 7)
+__version__ = Version("systembridgeconnector", 3, 0, 8, dev=0)
 __all__ = ["__version__"]
