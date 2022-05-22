7961d87cafc2e793b2d1fcb563f0d1f0225aac13
Aidan Timson <contact@timmo.xyz>
2022-05-22 16:35:57 +0100

    Remove header and footer

--- frontend/pages/app/data.tsx
+++ frontend/pages/app/data.tsx
@@ -10,6 +10,8 @@ function PageData(): ReactElement {
       title="Data"
       url="https://system-bridge.timmo.dev"
       description="Frontend for System Bridge"
+      noHeader
+      noFooter
     >
       <Container component="article" maxWidth="xl">
         <Data />
