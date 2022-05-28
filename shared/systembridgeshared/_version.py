474a270cbb11245d4429b6128ee89e54ae9dc0a4
Aidan Timson <contact@timmo.xyz>
2022-05-28 14:05:23 +0100

    Version 3.1.0

--- shared/systembridgeshared/_version.py
+++ shared/systembridgeshared/_version.py
@@ -7,5 +7,5 @@ Provides systembridgeshared version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgeshared", 3, 1, 0, dev=9)
+__version__ = Version("systembridgeshared", 3, 1, 0)
 __all__ = ["__version__"]
--- windowssensors/systembridgewindowssensors/_version.py
+++ windowssensors/systembridgewindowssensors/_version.py
@@ -7,5 +7,5 @@ Provides systembridgewindowssensors version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgewindowssensors", 3, 1, 0, dev=9)
+__version__ = Version("systembridgewindowssensors", 3, 1, 0)
 __all__ = ["__version__"]
