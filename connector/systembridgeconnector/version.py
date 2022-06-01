3eee85ab054006f10ae6615216a9b716e6084c2d
Aidan Timson <contact@timmo.xyz>
2022-06-01 12:51:08 +0100

    Connector - Version checker (#1729)

---
+++ connector/systembridgeconnector/version.py
@@ -0,0 +1,72 @@
+"""System Bridge Connector: Version"""
+from __future__ import annotations
+
+from aiohttp import ClientSession
+from pkg_resources import parse_version
+
+from systembridgeconnector.base import Base
+from systembridgeconnector.exceptions import ConnectionErrorException
+from systembridgeconnector.http_client import HTTPClient
+from systembridgeconnector.models.system import System
+
+SUPPORTED_VERSION = "3.1.1"
+
+
+class Version(Base):
+    """Version"""
+
+    def __init__(
+        self,
+        api_host: str,
+        api_port: int,
+        api_key: str,
+        session: ClientSession | None = None,
+    ) -> None:
+        """Initialize the client."""
+        self._http_client = HTTPClient(
+            api_host,
+            api_port,
+            api_key,
+            session,
+        )
+
+    async def check_supported(self) -> bool:
+        """Check if the system is running a supported version."""
+        if await self.check_version_2() is not None:
+            return True
+        version = await self.check_version_3()
+        if version is not None:
+            return parse_version(version) >= parse_version(SUPPORTED_VERSION)
+        return False
+
+    async def check_version_2(self) -> str | None:
+        """Check if the system is running v2.x.x version."""
+        try:
+            information = await self._http_client.get("/information")
+            if (
+                information
+                and information.get("version")
+                and (
+                    information.get("version").startswith("2")
+                    or information.get("version").startswith("v2")
+                )
+            ):
+                return information["version"]
+        except ConnectionErrorException:
+            pass
+        return None
+
+    async def check_version_3(self) -> str | None:
+        """Check if the system is running v3.x.x version."""
+        try:
+            response = await self._http_client.get("/api/system")
+            system = System(**response)
+            if (
+                system
+                and system.version
+                and parse_version(system.version) >= parse_version("3.0.0")
+            ):
+                return system.version
+        except ConnectionErrorException:
+            pass
+        return None
