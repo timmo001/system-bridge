89a0fd74ae2bfdbf272117497899fcf14c7aee8d
Aidan Timson <contact@timmo.xyz>
2022-06-13 14:52:58 +0100

    Fixes

--- backend/systembridgebackend/server/notification.py
+++ backend/systembridgebackend/server/notification.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import os
+import platform
 
 from plyer import notification
 from sanic.request import Request
@@ -11,20 +12,29 @@ from sanic.response import HTTPResponse, json
 def send_notification(
     message: str,
     title: str | None = None,
-    app_name: str = "System Bridge",
+    app_name: str | None = None,
     app_icon: str | None = None,
-    timeout: int = 5,
+    timeout: int | None = 5,
 ) -> None:
     """Send a notification."""
+    if title is None:
+        title = "System Bridge"
+    if app_name is None:
+        app_name = "System Bridge"
+    if app_icon is None:
+        app_icon = (
+            os.path.join(os.path.dirname(__file__), "../icon.ico")
+            if "Windows" in platform.system()
+            else os.path.join(os.path.dirname(__file__), "../icon.png")
+        )
+    if timeout is None:
+        timeout = 5
+
     notification.notify(
         title=title,
         message=message,
         app_name=app_name,
-        app_icon=app_icon
-        if app_icon is not None
-        else os.path.join(os.path.dirname(__file__), "../icon.ico")
-        if os.name == "nt"
-        else os.path.join(os.path.dirname(__file__), "../icon.png"),
+        app_icon=app_icon,
         timeout=timeout,
     )
 
