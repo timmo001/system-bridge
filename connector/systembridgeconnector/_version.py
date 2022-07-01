e0d06ee790e9f67aaaf560cb0c7bdc0b35a77f10
Aidan Timson <contact@timmo.xyz>
2022-07-01 10:03:28 +0100

    Version 3.3.1.dev0

--- connector/systembridgeconnector/_version.py
+++ connector/systembridgeconnector/_version.py
@@ -7,5 +7,5 @@ Provides systembridgeconnector version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgeconnector", 3, 3, 0)
+__version__ = Version("systembridgeconnector", 3, 3, 1, dev=0)
 __all__ = ["__version__"]
--- windowssensors/systembridgewindowssensors/_version.py
+++ windowssensors/systembridgewindowssensors/_version.py
@@ -7,5 +7,5 @@ Provides systembridgewindowssensors version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgewindowssensors", 3, 3, 0)
+__version__ = Version("systembridgewindowssensors", 3, 3, 1, dev=0)
 __all__ = ["__version__"]
