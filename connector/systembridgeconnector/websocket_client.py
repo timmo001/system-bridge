b3c6bddcc4da238c510108c6298db6ee942dc2b0
Aidan Timson <contact@timmo.xyz>
2022-07-06 11:47:31 +0100

    Websocket client response models with ids (#1827)

--- connector/systembridgeconnector/websocket_client.py
+++ connector/systembridgeconnector/websocket_client.py
@@ -1,9 +1,10 @@
 """System Bridge Connector: WebSocket Client"""
 from __future__ import annotations
 
+import asyncio
 from collections.abc import Callable
-import json
 import socket
+from uuid import uuid4
 
 import aiohttp
 
@@ -18,6 +19,7 @@ from systembridgeconnector.const import (
     EVENT_EVENT,
     EVENT_FILE,
     EVENT_FILES,
+    EVENT_ID,
     EVENT_KEY,
     EVENT_MESSAGE,
     EVENT_MODULE,
@@ -66,6 +68,8 @@ from systembridgeconnector.exceptions import (
     ConnectionClosedException,
     ConnectionErrorException,
 )
+from systembridgeconnector.models.request import Request
+from systembridgeconnector.models.response import Response
 
 
 class WebSocketClient(Base):
@@ -82,6 +86,7 @@ class WebSocketClient(Base):
         self._api_host = api_host
         self._api_port = api_port
         self._api_key = api_key
+        self._response_futures: dict[str, asyncio.Future] = {}
         self._session: aiohttp.ClientSession | None = None
         self._websocket: aiohttp.ClientWebSocketResponse | None = None
 
@@ -89,6 +94,23 @@ class WebSocketClient(Base):
     def connected(self) -> bool:
         """Get connection state."""
         return self._websocket is not None and not self._websocket.closed
+
+    async def _send_message(
+        self,
+        request: Request,
+    ) -> Response:
+        """Send a message to the WebSocket"""
+        if not self.connected or self._websocket is None:
+            raise ConnectionClosedException("Connection is closed")
+
+        request.id = uuid4().hex
+        future: asyncio.Future[Response] = asyncio.get_running_loop().create_future()
+        self._response_futures[request.id] = future
+        await self._websocket.send_str(request.json())
+        try:
+            return await future
+        finally:
+            self._response_futures.pop(request.id)
 
     async def close(self) -> None:
         """Close connection"""
@@ -127,182 +149,142 @@ class WebSocketClient(Base):
     async def application_update(
         self,
         version: str | None = None,
-    ) -> None:
+    ) -> Response:
         """Update application"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Updating application")
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_APPLICATION_UPDATE,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_VERSION: version,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_APPLICATION_UPDATE,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_VERSION: version,
+                }
             )
+        )
 
-    async def exit_backend(self) -> None:
+    async def exit_backend(self) -> Response:
         """Exit backend"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Exiting backend")
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_EXIT_APPLICATION,
-                        EVENT_API_KEY: self._api_key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_EXIT_APPLICATION,
+                    EVENT_API_KEY: self._api_key,
+                }
             )
+        )
 
     async def get_data(
         self,
         modules: list[str],
-    ) -> None:
+    ) -> Response:
         """Get data from server"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Getting data from server: %s", modules)
-
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_GET_DATA,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_MODULES: modules,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_GET_DATA,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_MODULES: modules,
+                }
             )
+        )
 
-    async def get_directories(self) -> None:
+    async def get_directories(self) -> Response:
         """Get directories"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Getting directories:")
-
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_GET_DIRECTORIES,
-                        EVENT_API_KEY: self._api_key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_GET_DIRECTORIES,
+                    EVENT_API_KEY: self._api_key,
+                }
             )
+        )
 
     async def get_files(
         self,
         base: str,
         path: str | None = None,
-    ) -> None:
+    ) -> Response:
         """Get files"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Getting files: %s - %s", base, path)
-
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_GET_FILES,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_BASE: base,
-                        EVENT_PATH: path,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_GET_FILES,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_BASE: base,
+                    EVENT_PATH: path,
+                }
             )
+        )
 
     async def get_file(
         self,
         base: str,
         path: str,
-    ) -> None:
+    ) -> Response:
         """Get files"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Getting file: %s - %s", base, path)
-
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_GET_FILE,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_BASE: base,
-                        EVENT_PATH: path,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_GET_FILE,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_BASE: base,
+                    EVENT_PATH: path,
+                }
             )
+        )
 
     async def register_data_listener(
         self,
         modules: list[str],
-    ) -> None:
+    ) -> Response:
         """Register data listener"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Registering data listener: %s", modules)
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_REGISTER_DATA_LISTENER,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_MODULES: modules,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_REGISTER_DATA_LISTENER,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_MODULES: modules,
+                }
             )
+        )
 
     async def keyboard_keypress(
         self,
         key: str,
-    ) -> None:
+    ) -> Response:
         """Keyboard keypress"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Press key: %s", key)
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_KEYBOARD_KEYPRESS,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_KEY: key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_KEYBOARD_KEYPRESS,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_KEY: key,
+                }
             )
+        )
 
     async def keyboard_text(
         self,
         text: str,
-    ) -> None:
+    ) -> Response:
         """Keyboard keypress"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Enter text: %s", text)
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_KEYBOARD_TEXT,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_TEXT: text,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_KEYBOARD_TEXT,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_TEXT: text,
+                }
             )
+        )
 
     async def send_notification(
         self,
@@ -311,162 +293,126 @@ class WebSocketClient(Base):
         app_name: str | None = None,
         app_icon: str | None = None,
         timeout: int = 5,
-    ) -> None:
+    ) -> Response:
         """Send notification"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Send notification: %s", message)
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_NOTIFICATION,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_MESSAGE: message,
-                        EVENT_TITLE: title,
-                        EVENT_APP_NAME: app_name,
-                        EVENT_APP_ICON: app_icon,
-                        EVENT_TIMEOUT: timeout,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_NOTIFICATION,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_MESSAGE: message,
+                    EVENT_TITLE: title,
+                    EVENT_APP_NAME: app_name,
+                    EVENT_APP_ICON: app_icon,
+                    EVENT_TIMEOUT: timeout,
+                }
             )
+        )
 
     async def open_path(
         self,
         path: str,
-    ) -> None:
+    ) -> Response:
         """Open path"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Opening path: %s", path)
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_OPEN,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_PATH: path,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_OPEN,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_PATH: path,
+                }
             )
+        )
 
     async def open_url(
         self,
         url: str,
-    ) -> None:
+    ) -> Response:
         """Open url"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Opening URL: %s", url)
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_OPEN,
-                        EVENT_API_KEY: self._api_key,
-                        EVENT_URL: url,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_OPEN,
+                    EVENT_API_KEY: self._api_key,
+                    EVENT_URL: url,
+                }
             )
+        )
 
-    async def power_sleep(self) -> None:
+    async def power_sleep(self) -> Response:
         """Power sleep"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Power sleep")
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_POWER_SLEEP,
-                        EVENT_API_KEY: self._api_key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_POWER_SLEEP,
+                    EVENT_API_KEY: self._api_key,
+                }
             )
+        )
 
-    async def power_hibernate(self) -> None:
+    async def power_hibernate(self) -> Response:
         """Power hibernate"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Power hibernate")
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_POWER_HIBERNATE,
-                        EVENT_API_KEY: self._api_key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_POWER_HIBERNATE,
+                    EVENT_API_KEY: self._api_key,
+                }
             )
+        )
 
-    async def power_restart(self) -> None:
+    async def power_restart(self) -> Response:
         """Power restart"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Power restart")
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_POWER_RESTART,
-                        EVENT_API_KEY: self._api_key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_POWER_RESTART,
+                    EVENT_API_KEY: self._api_key,
+                }
             )
+        )
 
-    async def power_shutdown(self) -> None:
+    async def power_shutdown(self) -> Response:
         """Power shutdown"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Power shutdown")
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_POWER_SHUTDOWN,
-                        EVENT_API_KEY: self._api_key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_POWER_SHUTDOWN,
+                    EVENT_API_KEY: self._api_key,
+                }
             )
+        )
 
-    async def power_lock(self) -> None:
+    async def power_lock(self) -> Response:
         """Power lock"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Power lock")
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_POWER_LOCK,
-                        EVENT_API_KEY: self._api_key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_POWER_LOCK,
+                    EVENT_API_KEY: self._api_key,
+                }
             )
+        )
 
-    async def power_logout(self) -> None:
+    async def power_logout(self) -> Response:
         """Power logout"""
-        if not self.connected:
-            raise ConnectionClosedException("Connection is closed")
-
         self._logger.info("Power logout")
-        if self._websocket is not None:
-            await self._websocket.send_str(
-                json.dumps(
-                    {
-                        EVENT_EVENT: TYPE_POWER_LOGOUT,
-                        EVENT_API_KEY: self._api_key,
-                    }
-                )
+        return await self._send_message(
+            Request(
+                **{
+                    EVENT_EVENT: TYPE_POWER_LOGOUT,
+                    EVENT_API_KEY: self._api_key,
+                }
             )
+        )
 
     async def listen(
         self,
@@ -478,6 +424,13 @@ class WebSocketClient(Base):
         async def _callback_message(message: dict) -> None:
             """Message Callback"""
             self._logger.debug("New message: %s", message[EVENT_TYPE])
+
+            if message.get(EVENT_ID) is not None:
+                future = self._response_futures.get(message[EVENT_ID])
+                if future is not None:
+                    self._logger.info("Responding to %s", message[EVENT_ID])
+                    future.set_result(Response(**message))
+
             if message[EVENT_TYPE] == TYPE_ERROR:
                 if message[EVENT_SUBTYPE] == SUBTYPE_LISTENER_ALREADY_REGISTERED:
                     self._logger.debug(message)
