a458700bd45a4d9d291ba70ee08a7686e045d929
Aidan Timson <contact@timmo.xyz>
2022-07-06 17:54:32 +0100

    Version 3.3.3.dev0

--- backend/systembridgebackend/_version.py
+++ backend/systembridgebackend/_version.py
@@ -7,5 +7,5 @@ Provides systembridgebackend version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgebackend", 3, 3, 2)
+__version__ = Version("systembridgebackend", 3, 3, 3, dev=0)
 __all__ = ["__version__"]
