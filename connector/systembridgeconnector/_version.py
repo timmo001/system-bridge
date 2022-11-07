12d57c182f1b47bf89e6bbe06f835e748fa977fa
timmo001 <timmo001@users.noreply.github.com>
2022-11-07 00:28:27 +0000

    Bump systembridgeconnector version to 3.5.0.dev1

--- connector/systembridgeconnector/_version.py
+++ connector/systembridgeconnector/_version.py
@@ -7,5 +7,5 @@ Provides systembridgeconnector version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgeconnector", 3, 5, 0, dev=0)
+__version__ = Version("systembridgeconnector", 3, 5, 0, dev=1)
 __all__ = ["__version__"]
