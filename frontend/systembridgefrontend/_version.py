8562db93107350f6a00939c26c5dc14acf4365ac
Aidan Timson <contact@timmo.xyz>
2022-12-18 16:14:54 +0000

    Version 3.6.2.dev0

--- frontend/systembridgefrontend/_version.py
+++ frontend/systembridgefrontend/_version.py
@@ -7,5 +7,5 @@ Provides systembridgefrontend version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgefrontend", 3, 6, 1)
+__version__ = Version("systembridgefrontend", 3, 6, 2, dev=0)
 __all__ = ["__version__"]
