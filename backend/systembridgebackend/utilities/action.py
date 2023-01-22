e73abf49fe7774aad3ba573199b31a6d6b965007
Aidan Timson <contact@timmo.xyz>
2023-01-22 14:33:38 +0000

    API Action

--- backend/systembridgebackend/utilities/action.py
+++ backend/systembridgebackend/utilities/action.py
@@ -2,25 +2,57 @@
 from typing import Any
 
 from systembridgeshared.base import Base
+from systembridgeshared.const import SECRET_API_KEY, SETTING_PORT_API
+from systembridgeshared.exceptions import (
+    AuthenticationException,
+    ConnectionErrorException,
+)
+from systembridgeshared.http_client import HTTPClient
 from systembridgeshared.models.action import Action
+from systembridgeshared.settings import Settings
 
 
 class ActionHandler(Base):
     """Handle actions"""
 
-    def handle(
+    def __init__(
+        self,
+        settings: Settings,
+    ) -> None:
+        """Initialize the action handler"""
+        super().__init__()
+        self._settings = settings
+
+    async def handle(
         self,
         action: Action,
     ) -> None:
         """Handle an action"""
         if action.command == "api" and action.data is not None:
-            self.api_action(action.data)
+            await self.api_action(action.data)
         else:
             self._logger.info("Unknown action: %s", action)
 
-    def api_action(
+    async def api_action(
         self,
         data: dict[str, Any],
     ) -> None:
         """Handle an API action"""
         self._logger.info("API Action: %s", data)
+
+        api_port = self._settings.get(SETTING_PORT_API)
+        api_key = self._settings.get(SECRET_API_KEY)
+        if api_port is None or api_key is None:
+            self._logger.warning("API not configured")
+            return
+        client = HTTPClient(
+            "localhost",
+            int(str(api_port)),
+            str(api_key),
+        )
+        try:
+            await client.request(data["method"], data["url"], *data)
+        except AuthenticationException as exception:
+            self._logger.warning("API authentication error: %s", exception)
+        except ConnectionErrorException as exception:
+            self._logger.warning("API connection error: %s", exception)
