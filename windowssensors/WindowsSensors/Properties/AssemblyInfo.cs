12948bf6e1e4af9ed56448a4c1a3581152d75ba1
Aidan Timson <contact@timmo.xyz>
2023-02-18 14:49:30 +0000

    Update .net framework to 4.8.1

--- windowssensors/WindowsSensors/Properties/AssemblyInfo.cs
+++ windowssensors/WindowsSensors/Properties/AssemblyInfo.cs
@@ -34,3 +34,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitleAttribute("System Bridge Windows Sensors")]
+[assembly: AssemblyDescriptionAttribute("System Bridge Windows Sensors")]
+[assembly: AssemblyCompanyAttribute("Timmo")]
+[assembly: AssemblyProductAttribute("System Bridge")]
