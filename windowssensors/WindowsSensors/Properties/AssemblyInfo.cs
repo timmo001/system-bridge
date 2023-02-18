18207e9909115c1152434615e993cf90f2bae808
Aidan Timson <contact@timmo.xyz>
2023-02-18 14:52:31 +0000

    Fix

--- windowssensors/WindowsSensors/Properties/AssemblyInfo.cs
+++ windowssensors/WindowsSensors/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -34,7 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyTitleAttribute("System Bridge Windows Sensors")]
-[assembly: AssemblyDescriptionAttribute("System Bridge Windows Sensors")]
-[assembly: AssemblyCompanyAttribute("Timmo")]
-[assembly: AssemblyProductAttribute("System Bridge")]
