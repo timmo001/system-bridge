722244b91f5e72f4461a42fc709de75e1435b5ac
Aidan Timson <atimson123@gmail.com>
2023-07-18 15:29:21 +0100

    Package with pyinstaller (#2576)
    * Package with pyinstaller
    
    * Dont create setup
    
    * use png for linux
    
    * Dont create setup
    
    * Fix
    
    * Fix pwsh syntax
    
    * Cleanup
    
    * Add typer
    
    * Edit command
    
    * Windowed
    
    * Package with pyinstaller
    
    * Dont create setup
    
    * use png for linux
    
    * Dont create setup
    
    * Fix
    
    * Fix pwsh syntax
    
    * Add typer
    
    * Edit command
    
    * Windowed
    
    * Order
    
    * Name
    
    * Oneliners
    
    * Add to deploy pipeline

--- systembridge.py
+++ systembridge.py
@@ -19,50 +19,46 @@ logger = setup_logger(LOG_LEVEL, "system-bridge")
 logging.getLogger("zeroconf").setLevel(logging.ERROR)
 
 
-@app.command(name=None, short_help="Main Application")
-def main(
+@app.command()
+def application(
+    type: str = typer.Argument("main", help="Application type"),
+    command: str = typer.Argument("main", help="Command"),
+    data: Optional[str] = typer.Argument(None, help="Data"),
     cli: bool = typer.Option(False, "--cli", help="CLI"),
     init: bool = typer.Option(False, "--init", help="Initialize"),
     no_frontend: bool = typer.Option(False, "--no-frontend", help="No Frontend"),
     no_gui: bool = typer.Option(False, "--no-gui", help="No GUI"),
 ) -> None:
-    """Main Application"""
+    """Application"""
     try:
-        from systembridgebackend import (
-            Application as BackendApplication,  # pylint: disable=import-outside-toplevel
-        )
+        if type == "gui":
+            from systembridgegui import (
+                Application as GUIApplication,  # pylint: disable=import-outside-toplevel
+            )
 
-        BackendApplication(
-            database,
-            settings,
-            cli=cli,
-            init=init,
-            no_frontend=no_frontend,
-            no_gui=no_gui,
-        )
+            setup_logger(LOG_LEVEL, "system-bridge-gui")
+            GUIApplication(
+                database,
+                settings,
+                command=command,
+                data=json.loads(data) if data else None,
+            )
+        else:
+            from systembridgebackend import (
+                Application as BackendApplication,  # pylint: disable=import-outside-toplevel
+            )
+
+            BackendApplication(
+                database,
+                settings,
+                cli=cli,
+                init=init,
+                no_frontend=no_frontend,
+                no_gui=no_gui,
+            )
     except Exception as exception:  # pylint: disable=broad-except
         logger.fatal("Unhandled error in application", exc_info=exception)
 
 
-@app.command(name="gui", short_help="GUI")
-def gui(
-    command: str = typer.Argument("main", help="Command"),
-    data: Optional[str] = typer.Argument(None, help="Data"),
-) -> None:
-    """GUI"""
-    try:
-        from systembridgegui import (
-            Application as GUIApplication,  # pylint: disable=import-outside-toplevel
-        )
-
-        GUIApplication(
-            database,
-            settings,
-            command=command,
-            data=json.loads(data) if data else None,
-        )
-    except Exception as exception:  # pylint: disable=broad-except
-        logger.fatal("Unhandled error in application", exc_info=exception)
-
-
-app()
+if __name__ == "__main__":
+    app()
