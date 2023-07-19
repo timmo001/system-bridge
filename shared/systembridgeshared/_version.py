2dd8b579994990f1144b1eb63be9f0490b00b33b
timmo001 <timmo001@users.noreply.github.com>
2023-07-19 08:21:38 +0000

    Bump systembridgeshared version to 3.8.0.dev23

--- shared/systembridgeshared/_version.py
+++ shared/systembridgeshared/_version.py
@@ -7,5 +7,5 @@ Provides systembridgeshared version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgeshared", 3, 8, 0, dev=22)
+__version__ = Version("systembridgeshared", 3, 8, 0, dev=23)
 __all__ = ["__version__"]
