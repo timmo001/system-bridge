b05b21139c5dab56f928cf1dc037f9e5ad2cf43c
timmo001 <timmo001@users.noreply.github.com>
2023-07-30 21:56:35 +0000

    Bump systembridgewindowssensors version to 3.8.0.dev37

--- windowssensors/systembridgewindowssensors/_version.py
+++ windowssensors/systembridgewindowssensors/_version.py
@@ -7,5 +7,5 @@ Provides systembridgewindowssensors version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgewindowssensors", 3, 8, 0, dev=36)
+__version__ = Version("systembridgewindowssensors", 3, 8, 0, dev=37)
 __all__ = ["__version__"]
