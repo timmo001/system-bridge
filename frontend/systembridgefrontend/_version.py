fb9763106de5dc31daf152346779fdb1cbf6fb43
timmo001 <timmo001@users.noreply.github.com>
2023-07-30 22:36:57 +0000

    Bump systembridgefrontend version to 3.8.0.dev38

--- frontend/systembridgefrontend/_version.py
+++ frontend/systembridgefrontend/_version.py
@@ -7,5 +7,5 @@ Provides systembridgefrontend version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgefrontend", 3, 8, 0, dev=37)
+__version__ = Version("systembridgefrontend", 3, 8, 0, dev=38)
 __all__ = ["__version__"]
