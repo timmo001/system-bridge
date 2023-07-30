1afb4c2672845ea1c4eca25b41dacbe15010fa45
timmo001 <timmo001@users.noreply.github.com>
2023-07-30 15:18:48 +0000

    Bump systembridgecli version to 3.8.0.dev30

--- cli/systembridgecli/_version.py
+++ cli/systembridgecli/_version.py
@@ -7,5 +7,5 @@ Provides systembridgecli version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgecli", 3, 8, 0, dev=29)
+__version__ = Version("systembridgecli", 3, 8, 0, dev=30)
 __all__ = ["__version__"]
