ee64d76a278667c959c7560bb5d4b4fac734ebee
Aidan Timson <contact@timmo.xyz>
2023-08-13 20:37:42 +0100

    3.8.2

--- shared/systembridgeshared/_version.py
+++ shared/systembridgeshared/_version.py
@@ -7,5 +7,5 @@ Provides systembridgeshared version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgeshared", 3, 8, 2, dev=5)
+__version__ = Version("systembridgeshared", 3, 8, 2)
 __all__ = ["__version__"]
