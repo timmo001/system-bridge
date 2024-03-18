50857fcd4faab1f81d459d977df460e9a1c26849
Aidan Timson <contact@timmo.xyz>
2024-03-18 11:06:32 +0000

    Setup frontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
