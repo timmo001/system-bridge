42c5acb86f7b24f32f9ca97db3bf0c1d4da4987a
Aidan Timson <aidan@timmo.dev>
2024-03-29 10:51:03 +0000

    Setup scripts (#2958)
    * Create setup scripts
    
    * Update README.md with development setup instructions

---
+++ setup.ps1
@@ -0,0 +1,38 @@
+pip install --upgrade setuptools wheel pyinstaller
+
+pip install -r requirements_windows.txt
+
+Invoke-WebRequest `
+  -Uri https://raw.githubusercontent.com/timmo001/system-bridge-backend/master/systembridgebackend/__main__.py `
+  -OutFile backend.py
+
+pyinstaller `
+  --clean `
+  --noconfirm `
+  --onedir `
+  --windowed `
+  --icon "resources/system-bridge.ico" `
+  --name "systembridgebackend" `
+  --collect-all "plyer" `
+  --collect-all "pywin32" `
+  --collect-all "winsdk" `
+  --collect-all "systembridgeshared" `
+  --collect-all "systembridgebackend" `
+  --collect-all "systembridgefrontend" `
+  --collect-all "systembridgewindowssensors" `
+  backend.py
+
+Invoke-WebRequest `
+  -Uri https://raw.githubusercontent.com/timmo001/system-bridge-cli/master/systembridgecli/__main__.py `
+  -OutFile cli.py
+
+pyinstaller `
+  --clean `
+  --noconfirm `
+  --onedir `
+  --windowed `
+  --icon "resources/system-bridge.ico" `
+  --name "systembridgecli" `
+  --collect-all "systembridgeshared" `
+  --collect-all "systembridgecli" `
+  cli.py
---
+++ setup.sh
@@ -0,0 +1,45 @@
+sudo apt-get update
+
+sudo apt-get install -y \
+  libayatana-appindicator3-dev \
+  libgtk-3-dev \
+  libjavascriptcoregtk-4.1-dev \
+  librsvg2-dev \
+  libsoup-3.0-dev \
+  libwebkit2gtk-4.1-dev
+
+pip install --upgrade setuptools wheel pyinstaller
+
+pip install -r requirements_linux.txt
+
+wget \
+  -O backend.py \
+  https://raw.githubusercontent.com/timmo001/system-bridge-backend/master/systembridgebackend/__main__.py
+
+pyinstaller \
+  --clean \
+  --noconfirm \
+  --onedir \
+  --windowed \
+  --icon "resources/system-bridge.png" \
+  --name "systembridgebackend" \
+  --collect-all "plyer" \
+  --collect-all "systembridgeshared" \
+  --collect-all "systembridgebackend" \
+  --collect-all "systembridgefrontend" \
+  backend.py
+
+wget \
+  -O cli.py \
+  https://raw.githubusercontent.com/timmo001/system-bridge-cli/master/systembridgecli/__main__.py
+
+pyinstaller \
+  --clean \
+  --noconfirm \
+  --onedir \
+  --windowed \
+  --icon "resources/system-bridge.png" \
+  --name "systembridgecli" \
+  --collect-all "systembridgeshared" \
+  --collect-all "systembridgecli" \
+  cli.py
