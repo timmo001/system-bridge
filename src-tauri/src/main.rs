0dc13743e29d8f9b59127f654ccb7664500ad72b
Aidan Timson <aidan@timmo.dev>
2024-03-31 12:57:47 +0100

    Add ability to disable backend or gui with command args (#2968)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,25 +18,56 @@ pub const BACKEND_HOST: &str = "127.0.0.1";
 
 #[tokio::main]
 async fn main() {
-    // Setup the backend server
-    let _handle = thread::spawn(move || {
-        let rt = Runtime::new().unwrap();
-        rt.block_on(async {
-            // Setup the backend server
-            setup_backend().await.unwrap();
+    let args: Vec<String> = std::env::args().collect();
 
-            // Check backend server is running every 60 seconds
-            let mut interval: tokio::time::Interval = interval(Duration::from_secs(60));
-            loop {
-                println!("Waiting for 60 seconds before checking the backend server again");
-                interval.tick().await;
+    // Parse the arguments
+    let mut no_backend = false;
+    let mut no_gui = false;
+    for arg in args.iter() {
+        if arg == "--no-backend" {
+            no_backend = true;
+        } else if arg == "--no-gui" {
+            no_gui = true;
+        }
+    }
 
+    if no_backend && no_gui {
+        println!("Both backend and GUI are disabled. Nothing to do");
+        std::process::exit(0);
+    }
+
+    if no_backend {
+        println!("Backend is disabled");
+    } else {
+        // Setup the backend server
+        let _handle = thread::spawn(move || {
+            let rt = Runtime::new().unwrap();
+            rt.block_on(async {
                 // Setup the backend server
                 setup_backend().await.unwrap();
-            }
-        });
-    });
 
-    // Setup the GUI
-    setup_gui().await;
+                // Check backend server is running every 60 seconds
+                let mut interval: tokio::time::Interval = interval(Duration::from_secs(60));
+                loop {
+                    println!("Waiting for 60 seconds before checking the backend server again");
+                    interval.tick().await;
+
+                    // Setup the backend server
+                    setup_backend().await.unwrap();
+                }
+            });
+        });
+    }
+
+    if no_gui {
+        println!("GUI is disabled");
+
+        // Wait forever
+        loop {
+            thread::park();
+        }
+    } else {
+        // Setup the GUI
+        setup_gui().await;
+    }
 }
