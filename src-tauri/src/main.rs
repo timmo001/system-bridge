2ea950a83c7fa5fd0af92e4c19f0e73f95c1b0ac
Aidan Timson <aidan@timmo.dev>
2024-03-31 12:46:14 +0100

    Move backend startup outside gui setup (#2967)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,11 +7,36 @@ mod gui;
 mod settings;
 mod websocket;
 
-use crate::gui::setup_gui;
+use std::thread;
+use std::time::Duration;
+use tokio::runtime::Runtime;
+use tokio::time::interval;
+
+use crate::{backend::setup_backend, gui::setup_gui};
 
 pub const BACKEND_HOST: &str = "127.0.0.1";
 
 #[tokio::main]
 async fn main() {
+    // Setup the backend server
+    let _handle = thread::spawn(move || {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            // Setup the backend server
+            setup_backend().await.unwrap();
+
+            // Check backend server is running every 60 seconds
+            let mut interval: tokio::time::Interval = interval(Duration::from_secs(60));
+            loop {
+                println!("Waiting for 60 seconds before checking the backend server again");
+                interval.tick().await;
+
+                // Setup the backend server
+                setup_backend().await.unwrap();
+            }
+        });
+    });
+
+    // Setup the GUI
     setup_gui().await;
 }
