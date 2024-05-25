7b4e84aad0c40bf3a003f6805afe8354568b531e
Aidan Timson <contact@timmo.xyz>
2024-05-25 17:21:54 +0100

    Add token api guard

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,9 +1,12 @@
-use log::info;
+use crate::settings::{get_settings, Settings};
+use log::{info, warn};
+use rocket::async_trait;
+use rocket::http::Status;
+use rocket::request::Outcome;
+use rocket::request::{FromRequest, Request};
 use rocket::serde::{json::Json, Serialize};
 use rocket::{catch, catchers, get, routes};
 use std::error::Error;
-
-use crate::settings::{get_settings, Settings};
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -38,8 +41,45 @@ pub fn root() -> Json<APIRoot> {
     Json(APIRoot { message: "Hello!" })
 }
 
+struct Token<'r>(&'r str);
+
+#[derive(Debug)]
+enum TokenError {
+    Missing,
+    Invalid,
+}
+
+#[async_trait]
+impl<'r> FromRequest<'r> for Token<'r> {
+    type Error = TokenError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        fn is_valid(token: &str) -> bool {
+            // Get settings
+            let settings: Settings = get_settings();
+
+            let valid = token == settings.api.token;
+
+            if !valid {
+                warn!(
+                    "Invalid token provided: {} - Expected: {}",
+                    token, settings.api.token
+                );
+            }
+
+            valid
+        }
+
+        match req.headers().get_one("token") {
+            None => Outcome::Error((Status::BadRequest, TokenError::Missing)),
+            Some(token) if is_valid(token) => Outcome::Success(Token(token)),
+            Some(_) => Outcome::Error((Status::BadRequest, TokenError::Invalid)),
+        }
+    }
+}
+
 #[get("/api", format = "json")]
-pub fn api() -> Json<APISubRoot> {
+fn api(_token: Token<'_>) -> Json<APISubRoot> {
     Json(APISubRoot {
         message: "Hello!",
         version: env!("CARGO_PKG_VERSION"),
