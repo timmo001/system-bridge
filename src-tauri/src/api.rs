c8f8046b69f14fe4f9eecf49ea637985c513a8a8
Aidan Timson <contact@timmo.xyz>
2024-05-25 16:54:08 +0100

    Add 404 catcher

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,13 +1,14 @@
 use log::info;
 use rocket::serde::{json::Json, Serialize};
-use rocket::{get, routes};
+use rocket::{catch, catchers, get, routes};
 use std::error::Error;
 
 use crate::settings::{get_settings, Settings};
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
-pub struct Root {
+pub struct APIError {
+    code: u16,
     message: &'static str,
 }
 
@@ -15,17 +16,31 @@ pub struct Root {
 #[serde(crate = "rocket::serde")]
 pub struct APIRoot {
     message: &'static str,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct APISubRoot {
+    message: &'static str,
     version: &'static str,
 }
 
+#[catch(404)]
+pub fn not_found() -> Json<APIError> {
+    Json(APIError {
+        code: 404,
+        message: "Not Found",
+    })
+}
+
 #[get("/", format = "json")]
-pub fn root() -> Json<Root> {
-    Json(Root { message: "Hello!" })
+pub fn root() -> Json<APIRoot> {
+    Json(APIRoot { message: "Hello!" })
 }
 
 #[get("/api", format = "json")]
-pub fn api() -> Json<APIRoot> {
-    Json(APIRoot {
+pub fn api() -> Json<APISubRoot> {
+    Json(APISubRoot {
         message: "Hello!",
         version: env!("CARGO_PKG_VERSION"),
     })
@@ -44,6 +59,7 @@ pub async fn setup_api() -> Result<(), Box<dyn Error>> {
 
     let _rocket = rocket::build()
         .configure(config)
+        .register("/", catchers![not_found])
         .mount("/", routes![root, api])
         .launch()
         .await?;
