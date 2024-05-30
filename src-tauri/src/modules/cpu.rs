1be94831039fe34a72775f8680ec9e12ad13411f
Aidan Timson <contact@timmo.xyz>
2024-05-30 10:59:55 +0100

    Use python module

--- src-tauri/src/modules/cpu.rs
+++ src-tauri/src/modules/cpu.rs
@@ -1,3 +1,5 @@
+use pyo3::prelude::*;
+use pyo3::FromPyObject;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use sysinfo::{Cpu, CpuRefreshKind, RefreshKind, System};
@@ -17,6 +19,22 @@ pub struct CPUStats {
     syscalls: Option<i32>,
 }
 
+impl<'source> FromPyObject<'source> for CPUStats {
+    fn extract(ob: &'source PyAny) -> PyResult<Self> {
+        let ctx_switches = ob.getattr("ctx_switches")?.extract::<i32>()?;
+        let interrupts = ob.getattr("interrupts")?.extract::<i32>()?;
+        let soft_interrupts = ob.getattr("soft_interrupts")?.extract::<i32>()?;
+        let syscalls = ob.getattr("syscalls")?.extract::<i32>()?;
+
+        Ok(CPUStats {
+            ctx_switches: Some(ctx_switches),
+            interrupts: Some(interrupts),
+            soft_interrupts: Some(soft_interrupts),
+            syscalls: Some(syscalls),
+        })
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CPUTimes {
     user: Option<f32>,
@@ -24,6 +42,24 @@ pub struct CPUTimes {
     idle: Option<f32>,
     interrupt: Option<f32>,
     dpc: Option<f32>,
+}
+
+impl<'source> FromPyObject<'source> for CPUTimes {
+    fn extract(ob: &'source PyAny) -> PyResult<Self> {
+        let user = ob.getattr("user")?.extract::<f32>()?;
+        let system = ob.getattr("system")?.extract::<f32>()?;
+        let idle = ob.getattr("idle")?.extract::<f32>()?;
+        let interrupt = ob.getattr("interrupt")?.extract::<f32>()?;
+        let dpc = ob.getattr("dpc")?.extract::<f32>()?;
+
+        Ok(CPUTimes {
+            user: Some(user),
+            system: Some(system),
+            idle: Some(idle),
+            interrupt: Some(interrupt),
+            dpc: Some(dpc),
+        })
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -113,16 +149,65 @@ pub async fn update() -> Result<Value, String> {
 
     let cpus = sys.cpus();
 
+    let (load_average, stats, times, times_percent) = Python::with_gil(|py| {
+        // Import the module
+        let cpu_module = PyModule::import_bound(py, "systembridgedata.module.cpu")
+            .expect("Failed to import systembridgedata.module.cpu module");
+
+        // Create an instance of the CPU class
+        let cpu_instance = cpu_module
+            .getattr("CPU")
+            .expect("Failed to get CPU class")
+            .call0()
+            .expect("Failed to create CPU instance");
+
+        // Call the methods
+        let load_average = cpu_instance
+            .getattr("get_load_average")
+            .expect("Failed to get get_load_average method")
+            .call0()
+            .expect("Failed to call get_load_average method")
+            .extract::<f32>()
+            .expect("Failed to extract string value from get_load_average result");
+
+        let stats = cpu_instance
+            .getattr("get_stats")
+            .expect("Failed to get get_stats method")
+            .call0()
+            .expect("Failed to call get_stats method")
+            .extract::<CPUStats>()
+            .expect("Failed to extract string value from get_stats result");
+
+        let times = cpu_instance
+            .getattr("get_times")
+            .expect("Failed to get get_times method")
+            .call0()
+            .expect("Failed to call get_times method")
+            .extract::<CPUTimes>()
+            .expect("Failed to extract string value from get_times result");
+
+        let times_percent = cpu_instance
+            .getattr("get_times_percent")
+            .expect("Failed to get get_times_percent method")
+            .call0()
+            .expect("Failed to call get_times_percent method")
+            .extract::<CPUTimes>()
+            .expect("Failed to extract string value from get_times_percent result");
+
+        // Set output
+        (load_average, stats, times, times_percent)
+    });
+
     Ok(serde_json::to_value(ModuleCPU {
         count: cpus.len(),
         frequency: get_frequency(cpu, None).unwrap(),
-        load_average: 0.0,
+        load_average,
         per_cpu: get_per_cpu(cpus).unwrap(),
         power: None,
-        stats: None,
+        stats: Some(stats),
         temperature: None,
-        times: None,
-        times_percent: None,
+        times: Some(times),
+        times_percent: Some(times_percent),
         usage: cpu.cpu_usage(),
         voltage: 0.0,
     })
