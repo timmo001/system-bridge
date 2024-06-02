477b04251706433ac05ff490054fb4f3fda99b16
Aidan Timson <aidan@timmo.dev>
2024-06-02 17:48:43 +0100

    Restore

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
