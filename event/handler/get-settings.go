d6c7fe8c44479f9add426c5a14f265c5be960a82
Aidan Timson <aidan@timmo.dev>
2025-03-26 00:17:15 +0000

    Get settings

--- event/handler/get-settings.go
+++ event/handler/get-settings.go
@@ -3,18 +3,28 @@ package event_handler
 import (
 	"github.com/charmbracelet/log"
 	"github.com/timmo001/system-bridge/event"
+	"github.com/timmo001/system-bridge/settings"
 )
 
 func RegisterGetSettingsHandler(router *event.MessageRouter) {
 	router.RegisterSimpleHandler(event.EventGetSettings, func(message event.Message) event.MessageResponse {
 		log.Infof("Received get settings event: %v", message)
 
-		// TODO: Implement
+		settings, err := settings.Load()
+		if err != nil {
+			return event.MessageResponse{
+				ID:      message.ID,
+				Type:    event.ResponseTypeError,
+				Subtype: event.ResponseSubtypeNone,
+				Message: "Failed to load settings",
+			}
+		}
+
 		return event.MessageResponse{
 			ID:      message.ID,
 			Type:    event.ResponseTypeSettingsResult,
 			Subtype: event.ResponseSubtypeNone,
-			Data:    message.Data,
+			Data:    settings,
 			Message: "Got settings",
 		}
 	})
