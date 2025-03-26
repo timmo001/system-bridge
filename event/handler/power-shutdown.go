93d7a1eaae21351b879f34a2bc5b268e5f0f692c
Aidan Timson <aidan@timmo.dev>
2025-03-26 08:24:22 +0000

    Power commands

--- event/handler/power-shutdown.go
+++ event/handler/power-shutdown.go
@@ -1,15 +1,44 @@
 package event_handler
 
 import (
+	"fmt"
+	"os/exec"
+	"runtime"
+	"time"
+
 	"github.com/charmbracelet/log"
 	"github.com/timmo001/system-bridge/event"
 )
+
+func Shutdown() error {
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("shutdown", "/s")
+		return cmd.Run()
+	case "linux":
+		cmd := exec.Command("systemctl", "poweroff")
+		return cmd.Run()
+	case "darwin":
+		cmd := exec.Command("osascript", "-e", "tell application \"System Events\" to shut down")
+		return cmd.Run()
+	default:
+		return fmt.Errorf("shutting down not supported on %s", runtime.GOOS)
+	}
+}
 
 func RegisterPowerShutdownHandler(router *event.MessageRouter) {
 	router.RegisterSimpleHandler(event.EventPowerShutdown, func(message event.Message) event.MessageResponse {
 		log.Infof("Received power shutdown event: %v", message)
 
-		// TODO: Implement
+		go func() {
+			time.Sleep(1 * time.Second)
+
+			// Shutdown the system
+			if err := Shutdown(); err != nil {
+				log.Errorf("Failed to shutdown system: %v", err)
+			}
+		}()
+
 		return event.MessageResponse{
 			ID:      message.ID,
 			Type:    event.ResponseTypePowerShuttingdown,
--- event/handler/power-sleep.go
+++ event/handler/power-sleep.go
@@ -1,15 +1,44 @@
 package event_handler
 
 import (
+	"fmt"
+	"os/exec"
+	"runtime"
+	"time"
+
 	"github.com/charmbracelet/log"
 	"github.com/timmo001/system-bridge/event"
 )
+
+func Sleep() error {
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("rundll32.exe", "powrprof.dll,SetSuspendState")
+		return cmd.Run()
+	case "linux":
+		cmd := exec.Command("systemctl", "suspend")
+		return cmd.Run()
+	case "darwin":
+		cmd := exec.Command("pmset", "sleepnow")
+		return cmd.Run()
+	default:
+		return fmt.Errorf("sleeping not supported on %s", runtime.GOOS)
+	}
+}
 
 func RegisterPowerSleepHandler(router *event.MessageRouter) {
 	router.RegisterSimpleHandler(event.EventPowerSleep, func(message event.Message) event.MessageResponse {
 		log.Infof("Received power sleep event: %v", message)
 
-		// TODO: Implement
+		go func() {
+			time.Sleep(1 * time.Second)
+
+			// Sleep the system
+			if err := Sleep(); err != nil {
+				log.Errorf("Failed to sleep system: %v", err)
+			}
+		}()
+
 		return event.MessageResponse{
 			ID:      message.ID,
 			Type:    event.ResponseTypePowerSleeping,
