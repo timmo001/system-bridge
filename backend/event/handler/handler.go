46075c90c3c208f863990726874e4022cf335906
Aidan Timson <aidan@timmo.dev>
2025-03-26 17:56:49 +0000

    Move handler registrations

---
+++ backend/event/handler/handler.go
@@ -0,0 +1,30 @@
+package event_handler
+
+import (
+	"github.com/timmo001/system-bridge/backend/event"
+)
+
+func RegisterHandlers(router *event.MessageRouter) {
+	RegisterDataUpdateHandler(router)
+	RegisterExitApplicationHandler(router)
+	RegisterGetDataHandler(router)
+	RegisterGetDirectoriesHandler(router)
+	RegisterGetFilesHandler(router)
+	RegisterGetFileHandler(router)
+	RegisterGetDirectoryHandler(router)
+	RegisterGetSettingsHandler(router)
+	RegisterKeyboardKeypressHandler(router)
+	RegisterKeyboardTextHandler(router)
+	RegisterMediaControlHandler(router)
+	RegisterNotificationHandler(router)
+	RegisterOpenHandler(router)
+	RegisterPowerHibernateHandler(router)
+	RegisterPowerLockHandler(router)
+	RegisterPowerLogoutHandler(router)
+	RegisterPowerRestartHandler(router)
+	RegisterPowerShutdownHandler(router)
+	RegisterPowerSleepHandler(router)
+	RegisterRegisterDataListenerHandler(router)
+	RegisterUnregisterDataListenerHandler(router)
+	RegisterUpdateSettingsHandler(router)
+}
