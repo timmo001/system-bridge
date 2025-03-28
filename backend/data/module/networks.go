40f492d25464e5ae2d83d2f7be8f16923dc766a7
Aidan Timson <aidan@timmo.dev>
2025-03-28 22:00:33 +0000

    Networks

--- backend/data/module/networks.go
+++ backend/data/module/networks.go
@@ -1,18 +1,11 @@
 package data_module
 
 import (
-	"github.com/charmbracelet/log"
+	"github.com/timmo001/system-bridge/backend/data/module/networks"
 	"github.com/timmo001/system-bridge/types"
 )
 
+// UpdateNetworksModule forwards the call to the networks module
 func (t *Module) UpdateNetworksModule() (types.NetworksData, error) {
-	log.Info("Getting networks data")
-
-	var networksData types.NetworksData
-	// Initialize arrays
-	networksData.Connections = make([]types.NetworkConnection, 0)
-	networksData.Networks = make([]types.Network, 0)
-
-	// TODO: Implement
-	return networksData, nil
+	return networks.UpdateNetworksModule()
 }
