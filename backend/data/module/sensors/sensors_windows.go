a00a92a09fe7b222d74428520a0ffbc6a62f88df
Aidan Timson <aidan@timmo.dev>
2025-03-28 23:24:35 +0000

    Run windows sensors

--- backend/data/module/sensors/sensors_windows.go
+++ backend/data/module/sensors/sensors_windows.go
@@ -4,51 +4,12 @@
 package sensors
 
 import (
+	"encoding/json"
+	"os/exec"
+
 	"github.com/charmbracelet/log"
 	"github.com/timmo001/system-bridge/types"
 )
-
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../lib/windows/librehardwaremonitor -L${SRCDIR}/../../../../lib/windows/nvidia -lLibreHardwareMonitorLib -lNvAPIWrapper
-// typedef struct {
-//     char id[256];
-//     char name[256];
-//     char sensor_type[64];
-//     double value;
-// } Sensor;
-//
-// typedef struct {
-//     char id[256];
-//     char name[256];
-//     char hardware_type[64];
-//     int sensor_count;
-//     int subhardware_count;
-// } Hardware;
-//
-// extern int InitializeHardwareMonitor();
-// extern int FinalizeHardwareMonitor();
-// extern int GetHardwareCount();
-// extern int GetHardware(int index, Hardware* hardware);
-// extern int GetSensorCount(int hardwareIndex);
-// extern int GetSensor(int hardwareIndex, int sensorIndex, Sensor* sensor);
-// extern int GetSubHardwareCount(int hardwareIndex);
-// extern int GetSubHardware(int hardwareIndex, int subhardwareIndex, Hardware* hardware);
-//
-// // NvAPI Functions
-// extern int InitializeNvAPI();
-// extern int FinalizeNvAPI();
-// extern int GetNvAPIChipset(int* id, char* name, char* flags, int* vendorId, char* vendorName);
-// extern int GetNvAPIDisplayCount();
-// extern int GetNvAPIDisplay(int index, int* id, char* name, int* active, int* available, int* connected,
-//                          int* dynamic, int* aspectH, int* aspectV, int* brightnessC, int* brightnessD,
-//                          int* brightnessMax, int* brightnessMin, char* colorDepth, char* connectionType,
-//                          int* pixelClock, int* refreshRate, int* resH, int* resV);
-// extern int GetNvAPIDriverInfo(char* branchVersion, char* interfaceVersion, int* version);
-// extern int GetNvAPIGPUCount();
-// extern int GetNvAPIGPU(int index, int* id, char* name, int* biosOEMRev, int* biosRev,
-//                      char* biosVersion, int* fanSpeedLevel, int* fanSpeedRPM,
-//                      int* driverModel, int* memAvail, int* memCapacity,
-//                      char* memMaker, char* serial, char* systemType, char* gpu_type);
-import "C"
 
 // getWindowsSensorsData fetches sensor data available only on Windows platforms
 func getWindowsSensorsData() (*types.SensorsWindows, error) {
@@ -61,8 +22,17 @@ func getWindowsSensorsData() (*types.SensorsWindows, error) {
 		GPUs:     make([]types.SensorsNVIDIAGPU, 0),
 	}
 
-	// Since we're using placeholder DLLs, we'll just return the empty structures for now
-	// The actual implementation will be completed when real DLLs are available
+	// Run lib/sensors/windows/bin/SystemBridgeWindowsSensors.exe
+	cmd := exec.Command("lib/sensors/windows/bin/SystemBridgeWindowsSensors.exe")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Debug("Windows sensors data", "data", string(output))
+
+	// Parse the output
+	json.Unmarshal(output, &windowsSensors)
 
 	return &windowsSensors, nil
 }
