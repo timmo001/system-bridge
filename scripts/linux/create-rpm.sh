0d40b3eb738d683884327eaef8077d41b12f6345
Aidan Timson <aidan@timmo.dev>
2025-03-30 20:13:34 +0100

    Fix RPM

--- scripts/linux/create-rpm.sh
+++ scripts/linux/create-rpm.sh
@@ -15,9 +15,11 @@ else
     RPM_RELEASE="1"
 fi
 
-# Create directory structure
+# Create directory structure in rpm-structure
 mkdir -p rpm-structure/usr/bin
 mkdir -p rpm-structure/usr/share/icons/hicolor/512x512/apps
+
+# Copy files to rpm-structure
 cp system-bridge-linux rpm-structure/usr/bin/system-bridge
 cp resources/system-bridge-circle.png rpm-structure/usr/share/icons/hicolor/512x512/apps/system-bridge.png
 
@@ -29,13 +31,14 @@ sed -e "s/%{_version}/$RPM_VERSION/g" \
     -e "s/%{_release}/$RPM_RELEASE/g" \
     "$(dirname "$0")/system-bridge.spec" >rpmbuild/SPECS/system-bridge.spec
 
-# Create BUILDROOT directory and copy files
-mkdir -p "rpmbuild/BUILDROOT/system-bridge-${RPM_VERSION}-${RPM_RELEASE}.x86_64/usr/bin"
-mkdir -p "rpmbuild/BUILDROOT/system-bridge-${RPM_VERSION}-${RPM_RELEASE}.x86_64/usr/share/icons/hicolor/512x512/apps"
+# Create BUILDROOT directory structure
+BUILDROOT_DIR="rpmbuild/BUILDROOT/system-bridge-${RPM_VERSION}-${RPM_RELEASE}.x86_64"
+mkdir -p "${BUILDROOT_DIR}/usr/bin"
+mkdir -p "${BUILDROOT_DIR}/usr/share/icons/hicolor/512x512/apps"
 
 # Copy files to BUILDROOT
-cp rpm-structure/usr/bin/system-bridge "rpmbuild/BUILDROOT/system-bridge-${RPM_VERSION}-${RPM_RELEASE}.x86_64/usr/bin/"
-cp rpm-structure/usr/share/icons/hicolor/512x512/apps/system-bridge.png "rpmbuild/BUILDROOT/system-bridge-${RPM_VERSION}-${RPM_RELEASE}.x86_64/usr/share/icons/hicolor/512x512/apps/"
+cp rpm-structure/usr/bin/system-bridge "${BUILDROOT_DIR}/usr/bin/"
+cp rpm-structure/usr/share/icons/hicolor/512x512/apps/system-bridge.png "${BUILDROOT_DIR}/usr/share/icons/hicolor/512x512/apps/"
 
 # Build the RPM package
 rpmbuild --define "_topdir $(pwd)/rpmbuild" \
