dac3be5e009084b02b70bd8c31e177727176c654
Aidan Timson <aidan@timmo.dev>
2025-03-30 20:18:53 +0100

    Icons

---
+++ scripts/linux/create-arch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Create build directory
+mkdir -p build/arch
+cd build/arch
+
+# Copy necessary files
+cp ../../system-bridge-linux .
+cp ../../scripts/linux/system-bridge.desktop .
+cp ../../LICENSE .
+cp ../../scripts/linux/PKGBUILD .
+
+# Copy icons
+cp ../../resources/system-bridge-circle.svg .
+cp ../../resources/system-bridge-circle-16x16.png .
+cp ../../resources/system-bridge-circle-32x32.png .
+cp ../../resources/system-bridge-circle.png .
+
+# Build package
+makepkg -f
+
+# Move package to dist directory
+mkdir -p ../../dist
+mv *.pkg.tar.zst ../../dist/
+
+cd ../..
+rm -rf build/arch
--- scripts/linux/create-deb.sh
+++ scripts/linux/create-deb.sh
@@ -7,9 +7,20 @@ VERSION=${VERSION:-0.0.0}
 # Create directory structure
 mkdir -p deb-structure/DEBIAN
 mkdir -p deb-structure/usr/bin
+mkdir -p deb-structure/usr/share/icons/hicolor/16x16/apps
+mkdir -p deb-structure/usr/share/icons/hicolor/32x32/apps
+mkdir -p deb-structure/usr/share/icons/hicolor/48x48/apps
+mkdir -p deb-structure/usr/share/icons/hicolor/128x128/apps
+mkdir -p deb-structure/usr/share/icons/hicolor/scalable/apps
 
 # Copy binary
 cp system-bridge-linux deb-structure/usr/bin/system-bridge
+
+# Copy icons
+cp resources/system-bridge-circle-16x16.png deb-structure/usr/share/icons/hicolor/16x16/apps/system-bridge.png
+cp resources/system-bridge-circle-32x32.png deb-structure/usr/share/icons/hicolor/32x32/apps/system-bridge.png
+cp resources/system-bridge-circle.png deb-structure/usr/share/icons/hicolor/128x128/apps/system-bridge.png
+cp resources/system-bridge-circle.svg deb-structure/usr/share/icons/hicolor/scalable/apps/system-bridge.svg
 
 # Create control file from template
 sed "s/\$VERSION/$VERSION/g" "$(dirname "$0")/control.template" >deb-structure/DEBIAN/control
