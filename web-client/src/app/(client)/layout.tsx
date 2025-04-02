a330a4cb58e3fe0ece8ec20dac86d0e6872273ee
Aidan Timson <aidan@timmo.dev>
2025-04-02 17:43:23 +0100

    Add to layout

--- web-client/src/app/(client)/layout.tsx
+++ web-client/src/app/(client)/layout.tsx
@@ -1,7 +1,23 @@
 "use client";
 
+import { useSystemBridgeConnectionStore } from "~/components/hooks/use-system-bridge-connection";
+import { useSystemBridgeWS } from "~/components/hooks/use-system-bridge-ws";
+
 export default function ClientLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <>{children}</>;
+  const { host, port } = useSystemBridgeConnectionStore();
+  const { isConnected } = useSystemBridgeWS();
+
+  return (
+    <>
+      {!host || !port ? (
+        <div>No connection</div>
+      ) : !isConnected ? (
+        <div>Connecting...</div>
+      ) : (
+        <>{children}</>
+      )}
+    </>
+  );
 }
