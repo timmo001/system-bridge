890ec4a5df197694a97e2531328a8f62bf87159d
Aidan Timson <aidan@timmo.dev>
2025-04-02 18:06:33 +0100

    Wait for hydration

--- web-client/src/app/(client)/layout.tsx
+++ web-client/src/app/(client)/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { SetupConnection } from "~/components/setup-connection";
 import { useSystemBridgeConnectionStore } from "~/components/hooks/use-system-bridge-connection";
 import { useSystemBridgeWS } from "~/components/hooks/use-system-bridge-ws";
@@ -7,12 +9,20 @@ import { useSystemBridgeWS } from "~/components/hooks/use-system-bridge-ws";
 export default function ClientLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const [isHydrated, setIsHydrated] = useState<boolean>(false);
+
   const { host, port, token } = useSystemBridgeConnectionStore();
   const { isConnected } = useSystemBridgeWS();
 
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+
   return (
     <>
-      {!host || !port || !token ? (
+      {!isConnected ? (
+        <div>Loading...</div>
+      ) : !host || !port || !token ? (
         <SetupConnection />
       ) : !isConnected ? (
         <div>Connecting...</div>
