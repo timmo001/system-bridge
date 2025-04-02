109d07e940a85d6e1cf673a15f587d394fae84a5
Aidan Timson <aidan@timmo.dev>
2025-04-02 17:44:36 +0100

    Fix

--- web-client/src/app/layout.tsx
+++ web-client/src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>
+    <html lang="en" className={`${geist.variable}`} suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
