a14292adb8a596d316429b7c8990d3e2cb545847
Aidan Timson <aidan@timmo.dev>
2025-05-31 13:03:19 +0100

    Override with nuqs

--- web-client/src/app/(websocket)/layout.tsx
+++ web-client/src/app/(websocket)/layout.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import {
+  parseAsBoolean,
+  parseAsInteger,
+  parseAsString,
+  useQueryState,
+} from "nuqs";
 
 import { SetupConnection } from "~/components/setup-connection";
 import { useSystemBridgeConnectionStore } from "~/components/hooks/use-system-bridge-connection";
@@ -9,13 +15,34 @@ import { SystemBridgeWSProvider } from "~/components/providers/system-bridge-ws-
 export default function WebSocketLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const [hostInput] = useQueryState("host", parseAsString);
+  const [portInput] = useQueryState("port", parseAsInteger);
+  const [apiKeyInput] = useQueryState("apiKey", parseAsString);
+  const [sslInput] = useQueryState("ssl", parseAsBoolean.withDefault(false));
+
   const [isHydrated, setIsHydrated] = useState<boolean>(false);
 
-  const { host, port, token } = useSystemBridgeConnectionStore();
+  const { host, port, token, setHost, setPort, setToken, setSsl } =
+    useSystemBridgeConnectionStore();
 
   useEffect(() => {
+    if (hostInput && portInput && apiKeyInput && sslInput) {
+      setHost(hostInput);
+      setPort(portInput);
+      setToken(apiKeyInput);
+      setSsl(sslInput);
+    }
     setIsHydrated(true);
-  }, []);
+  }, [
+    apiKeyInput,
+    hostInput,
+    portInput,
+    setHost,
+    setPort,
+    setSsl,
+    setToken,
+    sslInput,
+  ]);
 
   return (
     <SystemBridgeWSProvider>
