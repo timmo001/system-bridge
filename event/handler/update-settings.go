579e13bec2c3e339380dfdd067f082d708e276f8
Aidan Timson <aidan@timmo.dev>
2025-05-31 13:36:00 +0100

    Autostart setup

--- event/handler/update-settings.go
+++ event/handler/update-settings.go
@@ -1,6 +1,11 @@
 package event_handler
 
 import (
+	_ "embed"
+	"os"
+	"path/filepath"
+	"runtime"
+
 	"github.com/charmbracelet/log"
 	"github.com/mitchellh/mapstructure"
 	"github.com/timmo001/system-bridge/event"
@@ -10,6 +15,9 @@ import (
 type UpdateSettingsRequestData = settings.Settings
 
 type UpdateSettingsResponseData = settings.Settings
+
+//go:embed .scripts/linux/system-bridge.autostart.desktop
+var autostartDesktopFile []byte
 
 func RegisterUpdateSettingsHandler(router *event.MessageRouter) {
 	router.RegisterSimpleHandler(event.EventUpdateSettings, func(connection string, message event.Message) event.MessageResponse {
@@ -46,6 +54,38 @@ func RegisterUpdateSettingsHandler(router *event.MessageRouter) {
 				Type:    event.ResponseTypeError,
 				Subtype: event.ResponseSubtypeNone,
 				Message: "Failed to save settings",
+			}
+		}
+
+		if currentSettings.Autostart == false && newSettings.Autostart == true {
+			switch runtime.GOOS {
+			case "linux":
+				// Write autostart desktop file in ~/.config/autostart/system-bridge.desktop
+				err = os.WriteFile(filepath.Join(os.Getenv("HOME"), ".config", "autostart", "system-bridge.desktop"), autostartDesktopFile, 0644)
+				if err != nil {
+					log.Errorf("Failed to write autostart desktop file: %v", err)
+				}
+			default:
+				// case "windows":
+				// 	// TODO: Add to startup folder
+				// case "darwin":
+				// 	// TODO: Add to startup
+				log.Warnf("Autostart is not supported on %s", runtime.GOOS)
+			}
+		} else if currentSettings.Autostart == true && newSettings.Autostart == false {
+			switch runtime.GOOS {
+			case "linux":
+				// Remove autostart desktop file in ~/.config/autostart/system-bridge.desktop
+				err = os.Remove(filepath.Join(os.Getenv("HOME"), ".config", "autostart", "system-bridge.desktop"))
+				if err != nil {
+					log.Errorf("Failed to remove autostart desktop file: %v", err)
+				}
+			default:
+				// case "windows":
+				// 	// TODO: Remove from startup folder
+				// case "darwin":
+				// 	// TODO: Remove from startup
+				log.Warnf("Autostart is not supported on %s", runtime.GOOS)
 			}
 		}
 
