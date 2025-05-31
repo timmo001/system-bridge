b0e31e25373586145035078b1b465c10bffe4a02
Aidan Timson <aidan@timmo.dev>
2025-05-31 11:28:03 +0100

    Fixes

--- .scripts/linux/create-arch.sh
+++ .scripts/linux/create-arch.sh
@@ -13,7 +13,7 @@ mkdir -p build/arch
 cd build/arch
 
 # Copy necessary files
-cp ../../system-bridge-linux .
+cp ../../system-bridge-linux system-bridge
 cp ../../.scripts/linux/system-bridge.desktop .
 cp ../../LICENSE .
 cp ../../.scripts/linux/PKGBUILD .
@@ -23,6 +23,12 @@ cp ../../.resources/system-bridge-circle.svg .
 cp ../../.resources/system-bridge-circle-16x16.png .
 cp ../../.resources/system-bridge-circle-32x32.png .
 cp ../../.resources/system-bridge-circle.png .
+
+# Sanitize VERSION for Arch pkgver
+ARCH_PKGVER=$(echo "$VERSION" | sed 's/[-+]/./g')
+export ARCH_PKGVER
+
+echo "ARCH_PKGVER: $ARCH_PKGVER"
 
 # Build package
 makepkg -f
