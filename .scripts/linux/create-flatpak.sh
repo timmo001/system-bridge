9bcd679d45e0b144ac995b06de953d5e97cec017
Aidan Timson <aidan@timmo.dev>
2025-08-08 15:30:29 +0100

    Fix

--- .scripts/linux/create-flatpak.sh
+++ .scripts/linux/create-flatpak.sh
@@ -53,10 +53,10 @@ mkdir -p repo
 if command -v ostree &>/dev/null; then
   ostree --repo=repo init --mode=archive || true
   ostree --repo=repo config set core.min-free-space-percent 0 || true
-  ostree --repo=repo config set core.min-free-space-size 0 || true
+  ostree --repo=repo config set core.min-free-space-size 123MB || true
 else
   # This may fail on a fresh repo; ignore and continue, export will create it
-  flatpak build-update-repo --min-free-space-size=0MB --min-free-space-percent=0 repo || true
+  flatpak build-update-repo --min-free-space-size=123MB --min-free-space-percent=0 repo || true
 fi
 flatpak-builder --repo=repo --force-clean --disable-rofiles-fuse "$BUILD_DIR" "$(dirname "$0")/dev.timmo.system-bridge.yml"
 
