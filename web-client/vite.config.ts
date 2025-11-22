a6571e7ad3c72a0ae4eb131339a32c59d59ac64e
Aidan Timson <aidan@timmo.dev>
2025-11-22 16:03:41 +0000

    Add tailwind plugin

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  plugins: [tailwindcss()],
   build: {
     outDir: "dist",
     emptyOutDir: true,
