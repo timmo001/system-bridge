bab49ddabcb1a3afb306d374e79f540b61d02f58
Aidan Timson <aidan@timmo.dev>
2025-11-22 15:05:02 +0000

    Setup

---
+++ web-client/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./src"),
+    },
+  },
+});
