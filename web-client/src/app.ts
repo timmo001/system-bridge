38fe3a96d20bd6092500d014539e0f970c151d93
Aidan Timson <aidan@timmo.dev>
2025-11-22 15:55:16 +0000

    Replace router with modern impl

--- web-client/src/app.ts
+++ web-client/src/app.ts
@@ -1,51 +1,51 @@
+import "urlpattern-polyfill";
 import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators.js";
-import { Router } from "@vaadin/router";
+import { Routes } from "@lit-labs/router";
+import "./styles/globals.css";
 import "./components/theme-provider";
 import "./components/connection-provider";
 import "./components/websocket-provider";
 
 @customElement("app-root")
 export class App extends LitElement {
+  private routes = new Routes(this, [
+    {
+      path: "/",
+      render: () => html`<page-home></page-home>`,
+      enter: async () => {
+        await import("./pages/home");
+        return true;
+      },
+    },
+    {
+      path: "/connection",
+      render: () => html`<page-connection></page-connection>`,
+      enter: async () => {
+        await import("./pages/connection");
+        return true;
+      },
+    },
+    {
+      path: "/data",
+      render: () => html`<page-data></page-data>`,
+      enter: async () => {
+        await import("./pages/data");
+        return true;
+      },
+    },
+    {
+      path: "/settings",
+      render: () => html`<page-settings></page-settings>`,
+      enter: async () => {
+        await import("./pages/settings");
+        return true;
+      },
+    },
+  ]);
+
   protected createRenderRoot() {
     return this;
-  }
-
-  firstUpdated() {
-    const outlet = this.querySelector("#outlet");
-    if (!outlet) return;
-
-    const router = new Router(outlet);
-    router.setRoutes([
-      {
-        path: "/",
-        component: "page-home",
-        action: async () => {
-          await import("./pages/home");
-        },
-      },
-      {
-        path: "/connection",
-        component: "page-connection",
-        action: async () => {
-          await import("./pages/connection");
-        },
-      },
-      {
-        path: "/data",
-        component: "page-data",
-        action: async () => {
-          await import("./pages/data");
-        },
-      },
-      {
-        path: "/settings",
-        component: "page-settings",
-        action: async () => {
-          await import("./pages/settings");
-        },
-      },
-    ]);
   }
 
   render() {
@@ -53,7 +53,7 @@ export class App extends LitElement {
       <theme-provider>
         <connection-provider>
           <websocket-provider>
-            <div id="outlet"></div>
+            <main>${this.routes.outlet()}</main>
           </websocket-provider>
         </connection-provider>
       </theme-provider>
