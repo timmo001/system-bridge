0d5945a7a8a441c89a6b58fd0c0e61409a9cab47
Aidan Timson <aidan@timmo.dev>
2025-11-22 16:13:41 +0000

    Fix

---
+++ web-client/src/lib/system-bridge/types-settings.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+export const SettingsHotkeySchema = z.object({
+  name: z.string(),
+  key: z.string(),
+});
+
+export type SettingsHotkey = z.infer<typeof SettingsHotkeySchema>;
+
+export const SettingsMediaDirectorySchema = z.object({
+  name: z.string().min(1),
+  path: z.string().min(1),
+});
+
+export type SettingsMediaDirectory = z.infer<
+  typeof SettingsMediaDirectorySchema
+>;
+
+export const SettingsMediaSchema = z.object({
+  directories: z.array(SettingsMediaDirectorySchema),
+});
+
+export type SettingsMedia = z.infer<typeof SettingsMediaSchema>;
+
+export const SettingsSchema = z.object({
+  autostart: z.boolean(),
+  hotkeys: z.array(SettingsHotkeySchema),
+  logLevel: z.enum(["DEBUG", "INFO", "WARN", "ERROR"]),
+  media: SettingsMediaSchema,
+});
+
+export type Settings = z.infer<typeof SettingsSchema>;
---
+++ web-client/src/lib/system-bridge/types-websocket.ts
@@ -0,0 +1,115 @@
+import { z } from "zod";
+
+import { ModuleNameSchema } from "~/lib/system-bridge/types-modules";
+
+export const EventTypeSchema = z.enum([
+  "EXIT_APPLICATION",
+  "GET_DATA",
+  "GET_DIRECTORIES",
+  "GET_DIRECTORY",
+  "GET_FILES",
+  "GET_FILE",
+  "GET_SETTINGS",
+  "KEYBOARD_KEYPRESS",
+  "KEYBOARD_TEXT",
+  "MEDIA_CONTROL",
+  "NOTIFICATION",
+  "OPEN",
+  "POWER_HIBERNATE",
+  "POWER_LOCK",
+  "POWER_LOGOUT",
+  "POWER_RESTART",
+  "POWER_SHUTDOWN",
+  "POWER_SLEEP",
+  "REGISTER_DATA_LISTENER",
+  "UNREGISTER_DATA_LISTENER",
+  "DATA_UPDATE",
+  "UPDATE_SETTINGS",
+  "VALIDATE_DIRECTORY",
+]);
+
+export type EventType = z.infer<typeof EventTypeSchema>;
+
+export const ResponseTypeSchema = z.enum([
+  "ERROR",
+  "APPLICATION_EXITING",
+  "DATA_GET",
+  "DIRECTORIES",
+  "DIRECTORY",
+  "FILES",
+  "FILE",
+  "KEYBOARD_KEY_PRESSED",
+  "KEYBOARD_TEXT_SENT",
+  "MEDIA_CONTROLLED",
+  "NOTIFICATION_SENT",
+  "OPENED",
+  "POWER_HIBERNATING",
+  "POWER_LOCKING",
+  "POWER_LOGGINGOUT",
+  "POWER_RESTARTING",
+  "POWER_SHUTTINGDOWN",
+  "POWER_SLEEPING",
+  "DATA_LISTENER_REGISTERED",
+  "DATA_LISTENER_UNREGISTERED",
+  "DATA_UPDATE",
+  "SETTINGS_RESULT",
+  "SETTINGS_UPDATED",
+  "DIRECTORY_VALIDATED",
+]);
+
+export type ResponseType = z.infer<typeof ResponseTypeSchema>;
+
+export const ResponseSubtypeSchema = z.enum([
+  "NONE",
+  "BAD_REQUEST",
+  "BAD_TOKEN",
+  "BAD_JSON",
+  "BAD_DIRECTORY",
+  "BAD_FILE",
+  "BAD_PATH",
+  "INVALID_ACTION",
+  "LISTENER_ALREADY_REGISTERED",
+  "LISTENER_NOT_REGISTERED",
+  "MISSING_ACTION",
+  "MISSING_BASE",
+  "MISSING_KEY",
+  "MISSING_MODULES",
+  "MISSING_PATH",
+  "MISSING_PATH_URL",
+  "MISSING_SETTING",
+  "MISSING_TEXT",
+  "MISSING_TITLE",
+  "MISSING_TOKEN",
+  "MISSING_VALUE",
+  "UNKNOWN_EVENT",
+]);
+
+export type ResponseSubtype = z.infer<typeof ResponseSubtypeSchema>;
+
+export const WebSocketRequestSchema = z.object({
+  id: z.string(),
+  event: EventTypeSchema,
+  data: z.any().optional(),
+  token: z.string(),
+});
+
+export type WebSocketRequest = z.infer<typeof WebSocketRequestSchema>;
+
+export const WebSocketResponseSchema = z.object({
+  id: z.string(),
+  type: ResponseTypeSchema,
+  subtype: ResponseSubtypeSchema,
+  data: z.any(),
+  message: z.string().optional(),
+  module: ModuleNameSchema.optional(),
+});
+
+export type WebsocketResponse = z.infer<typeof WebSocketResponseSchema>;
+
+export const ValidateDirectoryResponseSchema = z.object({
+  valid: z.boolean(),
+});
+
+export type ValidateDirectoryResponse = z.infer<
+  typeof ValidateDirectoryResponseSchema
+>;
